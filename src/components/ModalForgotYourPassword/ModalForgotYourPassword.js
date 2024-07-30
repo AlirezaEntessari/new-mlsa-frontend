3d1cfba004e82febb667af025c7e9cb69b424f95
Alireza Entessari <alintssr@gmail.com>
2024-07-30 17:17:00 -0400

    Latest code

--- src/components/ModalForgotYourPassword/ModalForgotYourPassword.js
+++ src/components/ModalForgotYourPassword/ModalForgotYourPassword.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import './ModalForgotYourPassword.scss';
+import React from "react";
+import "./ModalForgotYourPassword.scss";
+import ClosePopUpWindowIcon from "../../assets/icons/XsmWindow.svg";
 
 export default function ModalForgotYourPassword() {
   return (
-    <div className='modal-forgot-your-password'>
-        <div className="modal-forgot-your-password__overlay">
-            <button className='modal-forgot-your-password__close-button'></button>
+    <div className="modal-forgot-your-password">
+      <div className="modal-forgot-your-password__overlay">
+        <div className="modal-forgot-your-password__content">
+          <button className="modal-forgot-your-password__close-button">
+            <img
+              className="modal-forgot-your-password__close-pop-up-window-icon"
+              src={ClosePopUpWindowIcon}
+              alt="Close Pop Up Window"
+            />
+          </button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
