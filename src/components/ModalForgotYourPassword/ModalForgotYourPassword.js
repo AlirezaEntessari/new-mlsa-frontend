f50709f1c1d9acc0bd48dd81704bc65fd52b0bd7
Alireza Entessari <alintssr@gmail.com>
2024-07-30 12:37:53 -0400

    Added Login Page

---
+++ src/components/ModalForgotYourPassword/ModalForgotYourPassword.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ModalForgotYourPassword.scss';
+
+export default function ModalForgotYourPassword() {
+  return (
+    <div className='modal-forgot-your-password'>
+        <div className="modal-forgot-your-password__overlay">
+            <button className='modal-forgot-your-password__close-button'></button>
+        </div>
+    </div>
+  )
+}
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,18 +1,47 @@
-import React from 'react'
-import './LoginPage.scss';
-import Header from '../../components/Header/Header';
+import React from "react";
+import "./LoginPage.scss";
+import Header from "../../components/Header/Header";
+import HideIcon from "../../assets/icons/ph_eye-light (1).svg";
+import CheckboxEmptyIcon from "../../assets/icons/CheckBoxEmpty.svg";
+import reCAPTCHAIcon from '../../assets/icons/reCAPTCHA.svg';
 
 export default function LoginPage() {
   return (
-    <div className='login'>
-        <Header />
-        <p className='login__header'>Welcome to MLSA</p>
-        <p className='login__enter-text'>Log in to enter</p>
-        <label className='login__email-label' htmlFor="email">Email</label>
-        <input className='login__email-input' type="text" id="email" placeholder='Enter your email' />
-        <label className='login__password-label' htmlFor="password">Password</label>
-        <input className='login__password-input' type="text" id="password" placeholder='Enter your password' />
-      
+    <div className="login">
+      <Header />
+      <p className="login__header">Welcome to MLSA</p>
+      <p className="login__enter-text">Log in to enter</p>
+      <label className="login__email-label" htmlFor="email">
+        Email
+      </label>
+      <input
+        className="login__email-input"
+        type="text"
+        id="email"
+        placeholder="Enter your email"
+      />
+      <img className="login__hide-icon" src={HideIcon} alt="Don't Show" />
+      <label className="login__password-label" htmlFor="password">
+        Password
+      </label>
+      <input
+        className="login__password-input"
+        type="text"
+        id="password"
+        placeholder="Enter your password"
+      />
+      <p className="login__keep-me-signed-in">
+        <img
+          className="login__checkbox-empty-icon"
+          src={CheckboxEmptyIcon}
+          alt="Check Box"
+        />
+        <span className="login__keep-me-signed-in-text">Keep me signed in</span>
+        <span className="login__forgot-your-password-text">Forgot your password?</span>
+      </p>
+      <img className="login__reCAPTCHA-icon" src={reCAPTCHAIcon} alt="reCAPTCHA Icon" />
+      <button className="login__enter-button">Enter</button>
+      <p className="login__dont-have-an-account">Don't have an account? <span className="login__sign-up-text">Sign Up</span></p>
     </div>
-  )
+  );
 }
