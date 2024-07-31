7b39e1a5ea8c4f1458eb5fd2f1268f44b07fb378
Alireza Entessari <alintssr@gmail.com>
2024-07-31 12:23:30 -0400

    Added Modal Forgot Your Password

--- src/components/ModalForgotYourPassword/ModalForgotYourPassword.js
+++ src/components/ModalForgotYourPassword/ModalForgotYourPassword.js
@@ -2,17 +2,44 @@ import React from "react";
 import "./ModalForgotYourPassword.scss";
 import ClosePopUpWindowIcon from "../../assets/icons/XsmWindow.svg";
 
-export default function ModalForgotYourPassword() {
+export default function ModalForgotYourPassword({ closeModal }) {
   return (
     <div className="modal-forgot-your-password">
-      <div className="modal-forgot-your-password__overlay">
-        <div className="modal-forgot-your-password__content">
-          <button className="modal-forgot-your-password__close-button">
+      <div className="modal-forgot-your-password__overlay" onClick={closeModal}>
+        <div
+          className="modal-forgot-your-password__content"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <button
+            className="modal-forgot-your-password__close-button"
+            onClick={closeModal}
+          >
             <img
               className="modal-forgot-your-password__close-pop-up-window-icon"
               src={ClosePopUpWindowIcon}
               alt="Close Pop Up Window"
             />
+          </button>
+          <p className="modal-forgot-your-password__header">
+            Forgot Your Password?
+          </p>
+          <p className="modal-forgot-your-password__instructions">
+            Enter your email address and we will send you instructions to reset
+            your password
+          </p>
+          <label
+            className="modal-forgot-your-password__email-label"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="modal-forgot-your-password__email-input"
+            type="text"
+            placeholder="Enter your email"
+          />
+          <button className="modal-forgot-your-password__continue-button">
+            Continue
           </button>
         </div>
       </div>
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginPage.scss";
 import Header from "../../components/Header/Header";
 import HideIcon from "../../assets/icons/ph_eye-light (1).svg";
 import CheckboxEmptyIcon from "../../assets/icons/CheckBoxEmpty.svg";
-import reCAPTCHAIcon from '../../assets/icons/reCAPTCHA.svg';
+import reCAPTCHAIcon from "../../assets/icons/reCAPTCHA.svg";
+import ModalForgotYourPassword from "../../components/ModalForgotYourPassword/ModalForgotYourPassword";
 
 export default function LoginPage() {
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const handleForgotPasswordClick = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   return (
     <div className="login">
       <Header />
@@ -37,11 +48,24 @@ export default function LoginPage() {
           alt="Check Box"
         />
         <span className="login__keep-me-signed-in-text">Keep me signed in</span>
-        <span className="login__forgot-your-password-text">Forgot your password?</span>
+        <span
+          className="login__forgot-your-password-text"
+          onClick={handleForgotPasswordClick}
+        >
+          Forgot your password?
+        </span>
       </p>
-      <img className="login__reCAPTCHA-icon" src={reCAPTCHAIcon} alt="reCAPTCHA Icon" />
+      <img
+        className="login__reCAPTCHA-icon"
+        src={reCAPTCHAIcon}
+        alt="reCAPTCHA Icon"
+      />
       <button className="login__enter-button">Enter</button>
-      <p className="login__dont-have-an-account">Don't have an account? <span className="login__sign-up-text">Sign Up</span></p>
+      <p className="login__dont-have-an-account">
+        Don't have an account?{" "}
+        <span className="login__sign-up-text">Sign Up</span>
+      </p>
+      {isModalVisible && <ModalForgotYourPassword closeModal={closeModal} />}
     </div>
   );
 }
