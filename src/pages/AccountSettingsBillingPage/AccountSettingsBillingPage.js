0db548de4db30f640aacd0002f59a102d037f42b
Alireza Entessari <alintssr@gmail.com>
2024-08-20 17:46:47 -0400

    Added Account Settings Billing Page Tablet styling

--- src/pages/AccountSettingsBillingPage/AccountSettingsBillingPage.js
+++ src/pages/AccountSettingsBillingPage/AccountSettingsBillingPage.js
@@ -20,16 +20,82 @@ export default function AccountSettingsBillingPage() {
             <p className="account-settings-billing__subscription-and-billing-settings">
               Subscription & Billing Settings
             </p>
-            <button className="account-settings-billing__cancel-button">Cancel</button>
-            <button className="account-settings-billing__save-changes-button">Save Changes</button>
+            <button className="account-settings-billing__cancel-button">
+              Cancel
+            </button>
+            <button className="account-settings-billing__save-changes-button">
+              Save Changes
+            </button>
           </div>
           <div className="account-settings-billing__subscription-container">
-            <p className="account-settings-billing__subscription-text">Subscription</p>
-            <p className="account-settings-billing__monthly-plan">Monthly Plan: $249/month billed on the 30th</p>
-            <p className="account-settings-billing__next-payment-due">Next Payment Due: 08/30/24</p>
-            <button className="account-settings-billing__upgrade-to-yearly-plan-button">Upgrade to Yearly Plan</button>
-            <p className="account-settings-billing__cancel-subscription">Cancel Subscription</p>
+            <p className="account-settings-billing__subscription-text">
+              Subscription
+            </p>
+            <p className="account-settings-billing__monthly-plan">
+              Monthly Plan:{" "}
+              <span className="account-settings-billing__monthly-plan-amount">
+                $249/month billed on the 30th
+              </span>
+            </p>
+            <p className="account-settings-billing__next-payment-due">
+              Next Payment Due:{" "}
+              <span className="account-settings-billing__next-payment-due-date"></span>{" "}
+              08/30/24
+            </p>
+            <button className="account-settings-billing__upgrade-to-yearly-plan-button">
+              Upgrade to Yearly Plan
+            </button>
+            <p className="account-settings-billing__cancel-subscription">
+              Cancel Subscription
+            </p>
           </div>
+          <div className="account-settings-billing__billing-container">
+            <p className="account-settings-billing__billing-text">Billing</p>
+            <p className="account-settings-billing__credit-card-on-file">
+              Credit Card on File:
+              <span className="account-settings-billing__credit-card-number">
+                XXXX-XXXX-XXXX-7890
+              </span>
+            </p>
+            <p className="account-settings-billing__last-payment-received">
+              Last Payment Received:{" "}
+              <span className="account-settings-billing__last-payment-received-date">
+                07/30/24
+              </span>
+            </p>
+            <button className="account-settings-billing__change-payment-method-button">
+              Change Payment Method
+            </button>
+            <p className="account-settings-billing__payment-history">
+              Payment History
+            </p>
+          </div>
+          <div className="account-settings-billing__referral-credits-container">
+            <p className="account-settings-billing__referral-credits-text">
+              Referral Credits
+            </p>
+            <p className="account-settings-billing__agency-referrals">
+              Agency Referrals
+            </p>
+            <p className="account-settings-billing__abc-staffing-inc">
+              ABC Staffing Inc.{" "}
+              <span className="account-settings-billing__abc-staffing-inc-date">
+                07/30/24
+              </span>
+            </p>
+            <p className="account-settings-billing__referral-credit">
+              Credit: $249 was credited to your account on 7/11/24
+            </p>
+            <button className="account-settings-billing__refer-an-agency-button">
+              Refer an Agency
+            </button>
+          </div>
+          <p className="account-settings-billing__questions-subscription-billing">
+            Questions about your Subscription or Billing?
+          </p>
+          <p className="account-settings-billing__email-us">
+            Email Us at getemailforstripesupport.com
+          </p>
         </div>
       </div>
     </div>
