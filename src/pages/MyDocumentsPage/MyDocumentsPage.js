65005e50d8945cf3b103fbef608b502446bd6d73
Alireza Entessari <alintssr@gmail.com>
2024-09-12 17:19:42 -0400

    Added Reports Page

--- src/pages/MyDocumentsPage/MyDocumentsPage.js
+++ src/pages/MyDocumentsPage/MyDocumentsPage.js
@@ -7,6 +7,12 @@ import SearchIcon from "../../assets/icons/Search (2).svg";
 import MyDocumentsNDANavbar from "../../components/MyDocumentsNDANavbar/MyDocumentsNDANavbar";
 import PDFLogo from "../../assets/icons/pdflogo.svg";
 import DownloadIcon from "../../assets/icons/downloadicon.svg";
+import CheckBoxEmptyIcon from "../../assets/icons/CheckBoxEmpty.svg";
+import PageArrowLeftIcon from "../../assets/icons/PageArrowLeft.svg";
+import PageArrowRightIcon from "../../assets/icons/PageArrowRight.svg";
+import PageNumberPeach1Icon from "../../assets/icons/PageNumberPeach1.svg";
+import PageNumberGray2Icon from "../../assets/icons/PageNumberGray2.svg";
+import PageNumberGray3Icon from "../../assets/icons/PageNumberGray3.svg";
 
 export default function MyDocumentsPage() {
   return (
@@ -343,6 +349,301 @@ export default function MyDocumentsPage() {
                 />
               </div>
             </div>
+          </div>
+          <div className="my-documents-page__tablet-row-containers">
+            <div className="my-documents-page__tablet-white-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-white-row-text">
+                7894982765758_Connor Matthews NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__white-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-gray-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-gray-row-text">
+                9347469298757_John Smith NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__gray-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-white-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-white-row-text">
+                7894982765758_Connor Matthews NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__white-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-gray-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-gray-row-text">
+                9347469298757_John Smith NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__gray-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-white-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-white-row-text">
+                7894982765758_Connor Matthews NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__white-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-gray-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-gray-row-text">
+                9347469298757_John Smith NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__gray-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-white-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-white-row-text">
+                7894982765758_Connor Matthews NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__white-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-gray-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-gray-row-text">
+                9347469298757_John Smith NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__gray-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-white-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-white-row-text">
+                7894982765758_Connor Matthews NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__white-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-gray-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-gray-row-text">
+                9347469298757_John Smith NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__gray-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-white-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-white-row-text">
+                7894982765758_Connor Matthews NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__white-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-gray-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-gray-row-text">
+                9347469298757_John Smith NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__gray-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-white-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-white-row-text">
+                7894982765758_Connor Matthews NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__white-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-gray-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-gray-row-text">
+                9347469298757_John Smith NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__gray-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-white-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-white-row-text">
+                7894982765758_Connor Matthews NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__white-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-gray-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-gray-row-text">
+                9347469298757_John Smith NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__gray-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-white-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-white-row-text">
+                7894982765758_Connor Matthews NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__white-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+            <div className="my-documents-page__tablet-gray-row-container">
+              <img
+                className="my-documents-page__pdf-logo-small"
+                src={PDFLogo}
+                alt="PDF Logo"
+              />
+              <span className="my-documents-page__tablet-gray-row-text">
+                9347469298757_John Smith NDA_signed_2024
+              </span>
+              <img
+                className="my-documents-page__gray-row-checkbox-empty-icon"
+                src={CheckBoxEmptyIcon}
+                alt="Checkbox Empty Icon"
+              />
+            </div>
+          </div>
+          <div className="my-documents-page__page-number-container">
+            <img
+              className="my-documents-page__page-arrow-left-icon"
+              src={PageArrowLeftIcon}
+              alt="Page Arrow Left Icon"
+            />
+            <img
+              className="my-documents-page__page-number-peach-1-icon"
+              src={PageNumberPeach1Icon}
+              alt="Page Number Peach 1 Icon"
+            />
+            <img
+              className="my-documents-page__page-number-gray-2-icon"
+              src={PageNumberGray2Icon}
+              alt="Page Number Gray 2 Icon"
+            />
+            <img
+              className="my-documents-page__page-number-gray-3-icon"
+              src={PageNumberGray3Icon}
+              alt="Page Number Gray 3 Icon"
+            />
+            <img className="my-documents-page__page-arrow-right-icon" src={PageArrowRightIcon} alt="Page Arrow Right Icon" />
           </div>
         </div>
       </div>
---
+++ src/pages/MyDocumentsReportsPage/MyDocumentsReportsPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './MyDocumentsReportsPage.scss';
+
+export default function MyDocumentsReportsPage() {
+  return (
+    <div className='my-documents-reports-page'>
+      
+    </div>
+  )
+}
