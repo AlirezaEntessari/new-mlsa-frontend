4b5589dfc33da2be062c029a4259c2e19803986b
Alireza Entessari <alintssr@gmail.com>
2024-09-18 14:22:05 -0400

    Added tablet styling to Job Report Page

--- src/pages/MyDocumentsJobReportPage/MyDocumentsJobReportPage.js
+++ src/pages/MyDocumentsJobReportPage/MyDocumentsJobReportPage.js
@@ -24,8 +24,10 @@ export default function MyDocumentsJobReportPage() {
         <DocumentsSidePanel />
         <div className="my-documents-job-report-page__right-container">
           <p className="my-documents-job-report-page__header">My Documents</p>
-          <GenerateReportHeader />
-          <GenerateReportOptions />
+          <div className="my-documents-job-report-page__mobile-report-container">
+            <GenerateReportHeader />
+            <GenerateReportOptions />
+          </div>
           <div className="my-documents-job-report-page__central-container">
             <label
               className="my-documents-job-report-page__client-name-label"
@@ -329,8 +331,8 @@ export default function MyDocumentsJobReportPage() {
                   name="experience-required"
                   id="experience-required"
                 >
-                  <option value="VISA" disabled selected>
-                    3-5 years
+                  <option value="3 - 5 years" disabled selected>
+                    3 - 5 years
                   </option>
                   <option value="0 - 1 years">0 - 1 years</option>
                   <option value="1 - 2 years">1 - 2 years</option>
@@ -520,6 +522,375 @@ export default function MyDocumentsJobReportPage() {
             >
               Generate Report
             </button>
+          </div>
+          <div className="my-documents-job-report-page__tablet-report-header-container">
+            <GenerateReportHeader />
+          </div>
+          <div className="my-documents-job-report-page__tablet-middle-container">
+            <GenerateReportOptions />
+            <div className="my-documents-job-report-page__tablet-central-container">
+              <div className="my-documents-job-report-page__tablet-client-name-container">
+                <label
+                  className="my-documents-job-report-page__tablet-client-name-label"
+                  htmlFor="tablet-client-name"
+                >
+                  Client Name
+                </label>
+                <input
+                  className="my-documents-job-report-page__tablet-client-name-input"
+                  type="text"
+                  id="tablet-client-name"
+                />
+              </div>
+              <div className="my-documents-job-report-page__tablet-job-container">
+                <label
+                  className="my-documents-job-report-page__tablet-job-title-label"
+                  htmlFor="tablet-job-title"
+                >
+                  Job Title
+                </label>
+                <input
+                  className="my-documents-job-report-page__tablet-job-title-input"
+                  type="text"
+                />
+              </div>
+              <div className="my-documents-job-report-page__tablet-job-type-status-environment-container">
+                <div className="my-documents-job-report-page__tablet-job-type-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-job-type-label"
+                    htmlFor="tablet-job-type"
+                  >
+                    Job Type
+                  </label>
+                  <select
+                    className="my-documents-job-report-page__tablet-job-type-select"
+                    name="tablet-job-type"
+                    id="tablet-job-type"
+                  >
+                    <option value="full-time" disabled selected>
+                      Full Time
+                    </option>
+                    <option value="part-time">Part Time</option>
+                    <option value="contract">Contract</option>
+                    <option value="traveling">Traveling</option>
+                  </select>
+                </div>
+                <div className="my-documents-job-report-page__tablet-job-status-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-job-status-label"
+                    htmlFor="tablet-job-status"
+                  >
+                    Job Status
+                  </label>
+                  <select
+                    className="my-documents-job-report-page__tablet-job-status-select"
+                    name="tablet-job-status"
+                    id="tablet-job-status"
+                  >
+                    <option value="expiring-soon" disabled selected>
+                      Expiring Soon
+                    </option>
+                    <option value="new job">New Job</option>
+                    <option value="hot job">Hot Job</option>
+                  </select>
+                </div>
+                <div className="my-documents-job-report-page__tablet-environment-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-environment-label"
+                    htmlFor="tablet-environment"
+                  >
+                    Environment
+                  </label>
+                  <select
+                    className="my-documents-job-report-page__tablet-environment-select"
+                    name="tablet-environment"
+                    id="tablet-environment"
+                  >
+                    <option value="on-site" disabled selected>
+                      On-Site
+                    </option>
+                    <option value="remote">Remote</option>
+                    <option value="traveling">Traveling</option>
+                  </select>
+                </div>
+              </div>
+              <div className="my-documents-job-report-page__tablet-industry-city-container">
+                <div className="my-documents-job-report-page__tablet-industry-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-industry-label"
+                    htmlFor="tablet-industry"
+                  >
+                    Industry
+                  </label>
+                  <select
+                    className="my-documents-job-report-page__tablet-industry-select"
+                    name="tablet-industry"
+                    id="tablet-industry"
+                  >
+                    <option
+                      value="professional-services-consulting"
+                      disabled
+                      selected
+                    >
+                      Professional Services/Consulting
+                    </option>
+                    <option value="aerospace-defense">
+                      Aerospace and Defense
+                    </option>
+                    <option value="agricultural">Agricultural</option>
+                    <option value="computers-and-technology">
+                      Computers and Technology
+                    </option>
+                    <option value="conglomerate">Conglomerate</option>
+                    <option value="consumer-goods">Consumer Goods</option>
+                    <option value="financial">Financial</option>
+                    <option value="government">Government</option>
+                    <option value="healthcare">Healthcare</option>
+                    <option value="insurance">Insurance</option>
+                    <option value="manufacturing">Manufacturing</option>
+                    <option value="oil-and-gas">Oil and Gas</option>
+                    <option value="pharmaceuticals">Pharmaceuticals</option>
+                    <option value="power-and-utilities">
+                      Power and Utilities
+                    </option>
+                    <option value="telecommunications">
+                      Telecommunications
+                    </option>
+                  </select>
+                </div>
+                <div className="my-documents-job-report-page__tablet-city-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-city-label"
+                    htmlFor="tablet-city"
+                  >
+                    City
+                  </label>
+                  <input
+                    className="my-documents-job-report-page__tablet-city-input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="my-documents-job-report-page__tablet-location-container">
+                <div className="my-documents-job-report-page__tablet-state-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-state-label"
+                    htmlFor="tablet-state"
+                  >
+                    State
+                  </label>
+                  <select
+                    className="my-documents-job-report-page__tablet-state-select"
+                    name="tablet-state"
+                    id="tablet-state"
+                  >
+                    <option value="Alabama" disabled selected>
+                      AL
+                    </option>
+                    <option value="Alaska">AK</option>
+                    <option value="Arizona">AZ</option>
+                    <option value="Arkansas">AR</option>
+                    <option value="California">CA</option>
+                    <option value="Colorado">CO</option>
+                    <option value="Connecticut">CT</option>
+                    <option value="Delaware">DE</option>
+                    <option value="Florida">FL</option>
+                    <option value="Georgia">GA</option>
+                    <option value="Hawaii">HI</option>
+                    <option value="Idaho">ID</option>
+                    <option value="Illinois">IL</option>
+                    <option value="Indiana">IN</option>
+                    <option value="Iowa">IA</option>
+                    <option value="Kansas">KS</option>
+                    <option value="Kentucky">KY</option>
+                    <option value="Louisiana">LA</option>
+                    <option value="Maine">ME</option>
+                    <option value="Maryland">MD</option>
+                    <option value="Massachusetts">MA</option>
+                    <option value="Michigan">MI</option>
+                    <option value="Minnesota">MN</option>
+                    <option value="Missouri">MO</option>
+                    <option value="Montana">MT</option>
+                    <option value="Nebraska">NE</option>
+                    <option value="Nevada">NV</option>
+                    <option value="New Hampshire">NH</option>
+                    <option value="New Jersey">NJ</option>
+                    <option value="New Mexico">NM</option>
+                    <option value="New York">NY</option>
+                    <option value="North Carolina">NC</option>
+                    <option value="North Dakota">ND</option>
+                    <option value="Ohio">OH</option>
+                    <option value="Oklahoma">OK</option>
+                    <option value="Oregon">OR</option>
+                    <option value="Pennsylvania">PA</option>
+                    <option value="Rhode Island">RI</option>
+                    <option value="South Carolina">SC</option>
+                    <option value="South Dakota">SD</option>
+                    <option value="Tennessee">TN</option>
+                    <option value="Texas">TX</option>
+                    <option value="Utah">UT</option>
+                    <option value="Vermont">VT</option>
+                    <option value="Virginia">VA</option>
+                    <option value="Washington">WA</option>
+                    <option value="West Virginia">WV</option>
+                    <option value="Wisconsin">WI</option>
+                    <option value="Wyoming">WY</option>
+                  </select>
+                </div>
+                <div className="my-documents-job-report-page__tablet-country-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-country-label"
+                    htmlFor="tablet-country"
+                  >
+                    Country
+                  </label>
+                  <select
+                    className="my-documents-job-report-page__tablet-country-select"
+                    name="tablet-country"
+                    id="tablet-country"
+                  >
+                    <option value="USA" disabled selected>
+                      USA
+                    </option>
+                    <option value="Canada">Canada</option>
+                    <option value="Mexico">Mexico</option>
+                    <option value="United Kingdom">United Kingdom</option>
+                    <option value="Spain">Spain</option>
+                    <option value="Germany">Germany</option>
+                  </select>
+                </div>
+                <div className="my-documents-job-report-page__tablet-zip-code-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-zip-code-label"
+                    htmlFor="tablet-zip-code"
+                  >
+                    Zip Code
+                  </label>
+                  <input
+                    className="my-documents-job-report-page__tablet-zip-code-input"
+                    type="text"
+                    id="tablet-zip-code"
+                  />
+                </div>
+              </div>
+              <div className="my-documents-job-report-page__tablet-openings-citizenship-experience-required-container">
+                <div className="my-documents-job-report-page__tablet-number-of-openings-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-number-of-openings-label"
+                    htmlFor="tablet-number-of-openings"
+                  >
+                    Number of Openings
+                  </label>
+                  <select
+                    className="my-documents-job-report-page__tablet-number-of-openings-select"
+                    name="tablet-number-of-openings"
+                    id="tablet-number-of-openings"
+                  >
+                    <option value="0" disabled selected>
+                      0
+                    </option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                  </select>
+                </div>
+                <div className="my-documents-job-report-page__tablet-citizenship-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-citizenship-label"
+                    htmlFor="tablet-citizenship"
+                  >
+                    Citizenship/Type of VISA
+                  </label>
+                  <select
+                    className="my-documents-job-report-page__tablet-citizenship-select"
+                    name="tablet-citizenship"
+                    id="tablet-citizenship"
+                  >
+                    <option value="VISA" disabled selected>
+                      VISA
+                    </option>
+                    <option value="US Citizen">US Citizen</option>
+                    <option value="Permanent Resident">
+                      Permanent Resident
+                    </option>
+                    <option value="OPT">OPT</option>
+                    <option value="H1B">H1B</option>
+                  </select>
+                </div>
+                <div className="my-documents-job-report-page__tablet-experience-required-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-experience-required-label"
+                    htmlFor="tablet-experience-required"
+                  >
+                    Experience Required
+                  </label>
+                  <select
+                    className="my-documents-job-report-page__tablet-experience-required-select"
+                    name="tablet-experience-required"
+                    id="tablet-experience-required"
+                  >
+                    <option value="0 - 1 years" disabled selected>
+                      0 - 1 years
+                    </option>
+                    <option value="1 - 2 years">1 - 2 years</option>
+                    <option value="2 - 3 years">2 - 3 years</option>
+                    <option value="3 - 4 years">3 - 4 years</option>
+                    <option value="4 - 5 years">4 - 5 years</option>
+                    <option value="5 - 6 years">5 - 6 years</option>
+                    <option value="6 - 7 years">6 - 7 years</option>
+                    <option value="7 - 8 years">7 - 8 years</option>
+                    <option value="8 - 9 years">8 - 9 years</option>
+                    <option value="9 - 10 years">9 - 10 years</option>
+                  </select>
+                </div>
+              </div>
+              <div className="my-documents-job-report-page__tablet-start-date-salary-type-range-container">
+                <div className="my-documents-job-report-page__tablet-start-date-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-start-date-label"
+                    htmlFor="tablet-start-date"
+                  >
+                    Start Date
+                  </label>
+                  <select
+                    className="my-documents-job-report-page__tablet-start-date-select"
+                    name="tablet-start-date"
+                    id="tablet-start-date"
+                  >
+                    <option value="01/01/25" disabled selected>
+                      01/01/25
+                    </option>
+                    <option value="01/02/25">01/02/25</option>
+                    <option value="01/03/25">01/03/25</option>
+                    <option value="01/04/25">01/04/25</option>
+                    <option value="01/05/25">01/05/25</option>
+                  </select>
+                </div>
+                <div className="my-documents-job-report-page__tablet-salary-type-container">
+                  <label
+                    className="my-documents-job-report-page__tablet-salary-type-label"
+                    htmlFor="tablet-salary-type"
+                  >
+                    Salary Type
+                  </label>
+                  <select name="tablet-salary-type" id="tablet-salary-type">
+                    <option value="Yearly" disabled selected>
+                      Yearly
+                    </option>
+                    <option value="weekly">Weekly</option>
+                    <option value="biweekly">Biweekly</option>
+                    <option value="monthly">Monthly</option>
+                  </select>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
