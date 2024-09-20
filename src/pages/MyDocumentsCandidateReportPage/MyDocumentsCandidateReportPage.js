74833b854c9d2512c8c0b91d9501611a7c247d0a
Alireza Entessari <alintssr@gmail.com>
2024-09-20 17:09:30 -0400

    Added styling

--- src/pages/MyDocumentsCandidateReportPage/MyDocumentsCandidateReportPage.js
+++ src/pages/MyDocumentsCandidateReportPage/MyDocumentsCandidateReportPage.js
@@ -557,8 +557,302 @@ export default function MyDocumentsCandidateReportPage() {
                   </select>
                 </div>
                 <div className="my-documents-candidate-report-page__tablet-city-container">
-                  <label className="my-documents-candidate-report-page__tablet-city-label" htmlFor="tablet-city">City</label>
-                  <input className="my-documents-candidate-report-page__tablet-city-input" type="text" />
+                  <label
+                    className="my-documents-candidate-report-page__tablet-city-label"
+                    htmlFor="tablet-city"
+                  >
+                    City
+                  </label>
+                  <input
+                    className="my-documents-candidate-report-page__tablet-city-input"
+                    type="text"
+                    id="tablet-city"
+                  />
+                </div>
+              </div>
+              <div className="my-documents-candidate-report-page__tablet-state-country-zip-code-container">
+                <div className="my-documents-candidate-report-page__tablet-state-container">
+                  <label
+                    className="my-documents-candidate-report-page__tablet-state-label"
+                    htmlFor="tablet-state-label"
+                  >
+                    State
+                  </label>
+                  <select
+                    className="my-documents-candidate-report-page__tablet-state-select"
+                    name="tablet-state"
+                    id="tablet-state"
+                  >
+                    <option value="Alabama" disabled selected>
+                      AL
+                    </option>
+                    <option value="Alaska">AK</option>
+                    <option value="Arizona">AZ</option>
+                    <option value="Arkansas">AR</option>
+                    <option value="California">CA</option>
+                    <option value="Colorado">CO</option>
+                    <option value="Connecticut">CT</option>
+                    <option value="Delaware">DE</option>
+                    <option value="Florida">FL</option>
+                    <option value="Georgia">GA</option>
+                    <option value="Hawaii">HI</option>
+                    <option value="Idaho">ID</option>
+                    <option value="Illinois">IL</option>
+                    <option value="Indiana">IN</option>
+                    <option value="Iowa">IA</option>
+                    <option value="Kansas">KS</option>
+                    <option value="Kentucky">KY</option>
+                    <option value="Louisiana">LA</option>
+                    <option value="Maine">ME</option>
+                    <option value="Maryland">MD</option>
+                    <option value="Massachusetts">MA</option>
+                    <option value="Michigan">MI</option>
+                    <option value="Minnesota">MN</option>
+                    <option value="Missouri">MO</option>
+                    <option value="Montana">MT</option>
+                    <option value="Nebraska">NE</option>
+                    <option value="Nevada">NV</option>
+                    <option value="New Hampshire">NH</option>
+                    <option value="New Jersey">NJ</option>
+                    <option value="New Mexico">NM</option>
+                    <option value="New York">NY</option>
+                    <option value="North Carolina">NC</option>
+                    <option value="North Dakota">ND</option>
+                    <option value="Ohio">OH</option>
+                    <option value="Oklahoma">OK</option>
+                    <option value="Oregon">OR</option>
+                    <option value="Pennsylvania">PA</option>
+                    <option value="Rhode Island">RI</option>
+                    <option value="South Carolina">SC</option>
+                    <option value="South Dakota">SD</option>
+                    <option value="Tennessee">TN</option>
+                    <option value="Texas">TX</option>
+                    <option value="Utah">UT</option>
+                    <option value="Vermont">VT</option>
+                    <option value="Virginia">VA</option>
+                    <option value="Washington">WA</option>
+                    <option value="West Virginia">WV</option>
+                    <option value="Wisconsin">WI</option>
+                    <option value="Wyoming">WY</option>
+                  </select>
+                </div>
+                <div className="my-documents-candidate-report-page__tablet-country-container">
+                  <label
+                    className="my-documents-candidate-report-page__tablet-country-label"
+                    htmlFor="tablet-country"
+                  >
+                    Country
+                  </label>
+                  <select
+                    className="my-documents-candidate-report-page__tablet-country-select"
+                    name="tablet-country"
+                    id="tablet-country"
+                  >
+                    <option value="USA" disabled selected>
+                      USA
+                    </option>
+                    <option value="Canada">Canada</option>
+                    <option value="Mexico">Mexico</option>
+                    <option value="United Kingdom">United Kingdom</option>
+                    <option value="Spain">Spain</option>
+                    <option value="Germany">Germany</option>
+                  </select>
+                </div>
+                <div className="my-documents-candidate-report-page__tablet-zip-code-container">
+                  <label
+                    className="my-documents-candidate-report-page__tablet-zip-code-label"
+                    htmlFor="tablet-zip-code"
+                  >
+                    Zip Code
+                  </label>
+                  <input
+                    className="my-documents-candidate-report-page__tablet-zip-code-input"
+                    type="text"
+                    id="tablet-zip-code"
+                  />
+                </div>
+              </div>
+              <div className="my-documents-candidate-report-page__tablet-number-of-openings-citizenship-experience-required-container">
+                <div className="my-documents-candidate-report-page__tablet-number-of-openings-container">
+                  <label
+                    className="my-documents-candidate-report-page__tablet-number-of-openings-label"
+                    htmlFor="tablet-number-of-openings"
+                  >
+                    Number of Openings
+                  </label>
+                  <select
+                    className="my-documents-candidate-report-page__tablet-number-of-openings-select"
+                    name="tablet-number-of-openings"
+                    id="tablet-number-of-openings"
+                  >
+                    <option value="0" disabled selected>
+                      0
+                    </option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                  </select>
+                </div>
+                <div className="my-documents-candidate-report-page__tablet-citizenship-container">
+                  <label
+                    className="my-documents-candidate-report-page__tablet-citizenship-label"
+                    htmlFor="tablet-citizenship"
+                  >
+                    Citizenship/Type of VISA
+                  </label>
+                  <select
+                    className="my-documents-candidate-report-page__tablet-citizenship-select"
+                    name="tablet-citizenship"
+                    id="tablet-citizenship"
+                  >
+                    <option value="VISA" disabled selected>
+                      VISA
+                    </option>
+                    <option value="US Citizen">US Citizen</option>
+                    <option value="Permanent Resident">
+                      Permanent Resident
+                    </option>
+                    <option value="OPT">OPT</option>
+                    <option value="H1B">H1B</option>
+                  </select>
+                </div>
+                <div className="my-documents-candidate-report-page__tablet-experience-required-container">
+                  <label
+                    className="my-documents-candidate-report-page__tablet-experience-required-label"
+                    htmlFor="tablet-experience-required"
+                  >
+                    Experience Required
+                  </label>
+                  <select
+                    className="my-documents-candidate-report-page__tablet-experience-required-select"
+                    name="tablet-experience-required"
+                    id="tablet-experience-required"
+                  >
+                    <option value="3 - 5 years" disabled selected>
+                      3 - 5 years
+                    </option>
+                    <option value="0 - 1 years">0 - 1 years</option>
+                    <option value="1 - 2 years">1 - 2 years</option>
+                    <option value="2 - 3 years">2 - 3 years</option>
+                    <option value="3 - 4 years">3 - 4 years</option>
+                    <option value="4 - 5 years">4 - 5 years</option>
+                    <option value="5 - 6 years">5 - 6 years</option>
+                    <option value="6 - 7 years">6 - 7 years</option>
+                    <option value="7 - 8 years">7 - 8 years</option>
+                    <option value="8 - 9 years">8 - 9 years</option>
+                    <option value="9 - 10 years">9 - 10 years</option>
+                  </select>
+                </div>
+              </div>
+              <div className="my-documents-candidate-report-page__tablet-start-date-salary-type-salary-range-container">
+                <div className="my-documents-candidate-report-page__tablet-start-date-container">
+                  <label
+                    className="my-documents-candidate-report-page__tablet-start-date-label"
+                    htmlFor="tablet-start-date"
+                  >
+                    Start Date
+                  </label>
+                  <select
+                    className="my-documents-candidate-report-page__tablet-start-date-select"
+                    name="tablet-start-date"
+                    id="tablet-start-date"
+                  >
+                    <option value="01/01/25" disabled selected>
+                      01/01/25
+                    </option>
+                    <option value="01/02/25">01/02/25</option>
+                    <option value="01/03/25">01/03/25</option>
+                    <option value="01/04/25">01/04/25</option>
+                    <option value="01/05/25"></option>
+                  </select>
+                </div>
+                <div className="my-documents-candidate-report-page__tablet-salary-type-container">
+                  <label
+                    className="my-documents-candidate-report-page__tablet-salary-type-label"
+                    htmlFor="tablet-salary-type"
+                  >
+                    Salary Type
+                  </label>
+                  <select
+                    className="my-documents-candidate-report-page__tablet-salary-type-select"
+                    name="tablet-salary-type"
+                    id="tablet-salary-type"
+                  >
+                    <option value="salary-type" disabled selected>
+                      Yearly
+                    </option>
+                    <option value="weekly">Weekly</option>
+                    <option value="bi-weekly">Bi-weekly</option>
+                    <option value="monthly">Monthly</option>
+                  </select>
+                </div>
+                <div className="my-documents-candidate-report-page__tablet-salary-range-first-container">
+                  <label
+                    className="my-documents-candidate-report-page__tablet-salary-range-first-label"
+                    htmlFor="tablet-salary-range-first"
+                  >
+                    Salary Range
+                  </label>
+                  <select
+                    className="my-documents-candidate-report-page__tablet-salary-range-first-select"
+                    name="tablet-salary-range-first"
+                    id="tablet-salary-range-first"
+                  >
+                    <option value="0" disabled selected>
+                      0
+                    </option>
+                    <option value="50,000">50,000</option>
+                    <option value="100,000">100,000</option>
+                    <option value="150,000">150,000</option>
+                    <option value="200,000">200,000</option>
+                    <option value="250,000">250,000</option>
+                    <option value="500,000+">500,000+</option>
+                  </select>
+                </div>
+                <div className="my-documents-candidate-report-page__tablet-salary-range-second-container">
+                  <select
+                    className="my-documents-candidate-report-page__tablet-salary-range-second-select"
+                    name="tablet-salary-range-second"
+                    id="tablet-salary-range-second"
+                  >
+                    <option value="50,000">50,000</option>
+                    <option value="100,000">100,000</option>
+                    <option value="150,000">150,000</option>
+                    <option value="200,000">200,000</option>
+                    <option value="250,000">250,000</option>
+                    <option value="500,000">500,000</option>
+                    <option value="500,000+">500,000+</option>
+                  </select>
+                </div>
+              </div>
+              <div className="my-documents-candidate-report-page__tablet-fee-type-fee-negotiable-guarantee-period-travel-required-container">
+                <div className="my-documents-candidate-report-page__tablet-fee-type-container">
+                  <label
+                    className="my-documents-candidate-report-page__tablet-fee-type-label"
+                    htmlFor="tablet-fee-type"
+                  >
+                    Fee Type
+                  </label>
+                  <select
+                    className="my-documents-candidate-report-page__tablet-fee-type-select"
+                    name="tablet-fee-type"
+                    id="tablet-fee-type"
+                  >
+                    <option disabled selected value="0% / 100%">
+                      0% / 100%
+                    </option>
+                    <option value="25% / 75%">25% / 75%</option>
+                    <option value="50% / 50%">50% / 50%</option>
+                    <option value="75% / 25%">75% / 25%</option>
+                    <option value="100% / 0%">100% / 0%</option>
+                  </select>
                 </div>
               </div>
             </div>
