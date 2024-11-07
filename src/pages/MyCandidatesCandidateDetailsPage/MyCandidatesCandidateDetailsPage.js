7236ed26e3c76aad97e5293d10c7df95380768b6
Alireza Entessari <alintssr@gmail.com>
2024-11-07 14:16:38 -0500

    Added tablet styling to My Candidates Candidate Details Page

--- src/pages/MyCandidatesCandidateDetailsPage/MyCandidatesCandidateDetailsPage.js
+++ src/pages/MyCandidatesCandidateDetailsPage/MyCandidatesCandidateDetailsPage.js
@@ -433,6 +433,305 @@ export default function MyCandidatesCandidateDetailsPage() {
                 </p>
               </div>
             </div>
+            <div className="my-candidates-candidate-details-page__tablet-mlsa-member-view-hot-candidate-put-on-hold-container">
+              <div className="my-candidates-candidate-details-page__tablet-mlsa-member-view-container">
+                <img
+                  className="my-candidates-candidate-details-page__tablet-menu-arrow-circle-down-icon"
+                  src={MenuArrowCircleDownIcon}
+                  alt="Menu Arrow Circle Down Icon"
+                />
+                <p className="my-candidates-candidate-details-page__tablet-mlsa-member-view-text">
+                  MLSA Member View
+                </p>
+              </div>
+              <div className="my-candidates-candidate-details-page__tablet-hot-candidate-container">
+                <img
+                  className="my-candidates-candidate-details-page__tablet-circle-button-on-blue-icon"
+                  src={CircleButtonOnBlueIcon}
+                  alt="Circle Button On Blue Icon"
+                />
+                <img
+                  className="my-candidates-candidate-details-page__tablet-hot-jobs-on-icon"
+                  src={HotJobsOnIcon}
+                  alt="Hot Jobs On Icon"
+                />
+                <p className="my-candidates-candidate-details-page__tablet-hot-candidate-text">
+                  Hot Candidate
+                </p>
+              </div>
+              <div className="my-candidates-candidate-details-page__tablet-put-on-hold-container">
+                <img
+                  className="my-candidates-candidate-details-page__tablet-on-hold-button-off-icon"
+                  src={OnHoldButtonOffIcon}
+                  alt="On Hold Button Off Icon"
+                />
+                <img
+                  className="my-candidates-candidate-details-page__tablet-on-hold-off-icon"
+                  src={OnHoldOffIcon}
+                  alt="On Hold Off Icon"
+                />
+                <p className="my-candidates-candidate-details-page__tablet-put-on-hold-text">
+                  Put On Hold
+                </p>
+              </div>
+            </div>
+            <div className="my-candidates-candidate-details-page__tablet-hot-candidate-section">
+              <p className="my-candidates-candidate-details-page__tablet-hot-candidate-header">
+                <img
+                  className="my-candidates-candidate-details-page__tablet-hot-candidate-section-hot-jobs-on-icon"
+                  src={HotJobsOnIcon}
+                  alt="Hot Jobs On Icon"
+                />{" "}
+                <span className="my-candidates-candidate-details-page__tablet-hot-candidate-section-header-text">
+                  Hot Candidate
+                </span>
+              </p>
+              <div className="my-candidates-candidate-details-page__tablet-hot-candidate-section-information-container">
+                <div className="my-candidates-candidate-details-page__tablet-hot-candidate-section-left-container">
+                  <p className="my-candidates-candidate-details-page__tablet-hot-candidate-section-candidate-id">
+                    Candidate ID# 93782756
+                  </p>
+                  <p className="my-candidates-candidate-details-page__tablet-hot-candidate-section-job-title">
+                    Mechanical Engineer Designer
+                  </p>
+                  <p className="my-candidates-candidate-details-page__tablet-hot-candidate-section-posted-on">
+                    Posted on: 08/07/2024
+                  </p>
+                  <p className="my-candidates-candidate-details-page__tablet-hot-candidate-section-duration">
+                    Duration: 13 weeks
+                  </p>
+                  <p className="my-candidates-candidate-details-page__tablet-hot-candidate-section-availability-date">
+                    Availability Date: 09/15/2024
+                  </p>
+                  <p className="my-candidates-candidate-details-page__tablet-hot-candidate-section-salary">
+                    Salary $110,000 - 145,000 Yearly
+                  </p>
+                </div>
+                <div className="my-candidates-candidate-details-page__tablet-hot-candidate-section-right-container">
+                  <div className="my-candidates-candidate-details-page__tablet-hot-candidate-section-staffing-agency-container">
+                    <p className="my-candidates-candidate-details-page__tablet-hot-candidate-section-staffing-agency-header">
+                      Staffing Agency
+                    </p>
+                    <p className="my-candidates-candidate-details-page__tablet-hot-candidate-section-staffing-agency-name">
+                      XYZ Staffing Inc.
+                    </p>
+                    <p className="my-candidates-candidate-details-page__tablet-hot-candidate-section-staffing-agency-location">
+                      San Antonio, Texas
+                    </p>
+                    <p className="my-candidates-candidate-details-page__tablet-hot-candidate-section-staffing-agency-phone-number">
+                      561-123-4567
+                    </p>
+                  </div>
+                  <button className="my-candidates-candidate-details-page__tablet-view-resume-button">
+                    View Resume
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="my-candidates-candidate-details-page__tablet-candidate-description-container">
+              <p className="my-candidates-candidate-details-page__tablet-candidate-description-header">
+                Candidate Description
+              </p>
+              <p className="my-candidates-candidate-details-page__tablet-candidate-description-description">
+                As a seasoned Mechanical Engineer Designer, this candidate
+                brings a wealth of experience in mechanical drafting and
+                industrial design, particularly within bulk material systems.
+                With a strong command of AutoCAD and a keen eye for detail, they
+                excel in creating precise, efficient diagrams and drafts. Their
+                ability to identify improvements and collaborate effectively
+                ensures that even the most complex designs are expertly crafted
+                and ready for seamless field installation. Driven by deadlines
+                and committed to quality, this candidate is perfectly suited to
+                enhance our team and contribute to our success.
+              </p>
+            </div>
+            <div className="my-candidates-candidate-details-page__tablet-professional-experience-container">
+              <p className="my-candidates-candidate-details-page__tablet-professional-experience-header">
+                Professional Experience
+              </p>
+              <ul className="my-candidates-candidate-details-page__tablet-professional-experience-list">
+                <li className="my-candidates-candidate-details-page__tablet-professional-experience-first-bullet">
+                  {" "}
+                  Designed and drafted over 50 complex bulk material handling
+                  systems, ensuring accuracy and adherence to industry
+                  standards.
+                </li>
+                <li className="my-candidates-candidate-details-page__tablet-professional-experience-second-bullet">
+                  Successfully improved system efficiency by 15% through
+                  strategic modifications in design and layout.
+                </li>
+                <li className="my-candidates-candidate-details-page__tablet-professional-experience-third-bullet">
+                  Led cross functional teams in the review and optimization of
+                  mechanical designs, resulting in smoother installation
+                  processes.
+                </li>
+                <li className="my-candidates-candidate-details-page__tablet-professional-experience-fourth-bullet">
+                  Managed multiple high-priority projects simultaneously,
+                  consistently delivering detailed AutoCAD drawings ahead of
+                  deadlines.
+                </li>
+                <li className="my-candidates-candidate-details-page__tablet-professional-experience-fifth-bullet">
+                  Provided ongoing technical support and updates during the
+                  installation phase, minimizing disruptions and ensuring
+                  project sucess.
+                </li>
+              </ul>
+            </div>
+            <div className="my-candidates-candidate-details-page__tablet-details-general-information-container">
+              <div className="my-candidates-candidate-details-page__tablet-details-container">
+                <p className="my-candidates-candidate-details-page__tablet-details-header">
+                  Details
+                </p>
+                <ul className="my-candidates-candidate-details-page__tablet-details-list">
+                  <li className="my-candidates-candidate-details-page__tablet-details-list-first-bullet">
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-seniority-level">
+                      Seniority Level:
+                    </span>
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-seniority-level-status">
+                      Entry Level
+                    </span>
+                  </li>
+                  <li className="my-candidates-candidate-details-page__tablet-details-list-second-bullet">
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-employment-type">
+                      Employment Type:
+                    </span>
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-employment-type-status">
+                      Full-time
+                    </span>
+                  </li>
+                  <li className="my-candidates-candidate-details-page__tablet-details-list-third-bullet">
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-job-function">
+                      Job function:
+                    </span>
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-job-function-status">
+                      Engineering, Information Technology
+                    </span>
+                  </li>
+                  <li className="my-candidates-candidate-details-page__tablet-details-list-fourth-bullet">
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-industries">
+                      Industries:
+                    </span>
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-industries-status">
+                      Industrial Machinery Manufacturing
+                    </span>
+                  </li>
+                  <li className="my-candidates-candidate-details-page__tablet-details-list-fifth-bullet">
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-citizenship">
+                      Citizenship:
+                    </span>
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-citizenship-status">
+                      US Citizen
+                    </span>
+                  </li>
+                  <li className="my-candidates-candidate-details-page__tablet-details-list-sixth-bullet">
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-environment">
+                      Environment:
+                    </span>
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-environment-status">
+                      Remote
+                    </span>
+                  </li>
+                  <li className="my-candidates-candidate-details-page__tablet-details-list-seventh-bullet">
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-travel">
+                      Travel:
+                    </span>
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-travel-status">
+                      Open to travel
+                    </span>
+                  </li>
+                  <li className="my-candidates-candidate-details-page__tablet-details-list-eighth-bullet">
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-other">
+                      Other:
+                    </span>
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-other-status">
+                      Paid Relocation requested
+                    </span>
+                  </li>
+                </ul>
+                <p className="my-candidates-candidate-details-page__tablet-details-list-bonus-text">
+                  Bonus
+                </p>
+              </div>
+              <div className="my-candidates-candidate-details-page__tablet-details-list-general-information-container">
+                <div className="my-candidates-candidate-details-page__tablet-details-list-general-information-inner-container">
+                  <p className="my-candidates-candidate-details-page__tablet-details-list-fee-type">
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-fee-type-text">
+                      Fee Type:
+                    </span>
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-percentage-of-salary">
+                      Percentage of Salary
+                    </span>
+                  </p>
+                  <p className="my-candidates-candidate-details-page__tablet-details-list-split-fee-percentage">
+                    <span className="my-candidates-candidate-details-page__tablet-detailst-list-split-fee-percentage-text">
+                      Split Fee Percentage:
+                    </span>
+                    <span className="my-candidates-candidate-details-page__tablet-details-list-split-fee-percentage-percentage">
+                      50% 50%
+                    </span>
+                  </p>
+                  <p className="my-candidates-candidate-details-page__tablet-willing-to-negotiate">
+                    *Willing to negotiate
+                  </p>
+                  <p className="my-candidates-candidate-details-page__tablet-placement-fee">
+                    <span className="my-candidates-candidate-details-page__tablet-placement-fee-text">
+                      Placement Fee:
+                    </span>
+                    <span className="my-candidates-candidate-details-page__tablet-placement-fee-percentage">
+                      20%
+                    </span>
+                  </p>
+                  <p className="my-candidates-candidate-details-page__tablet-guarantee-period">
+                    <span className="my-candidates-candidate-details-page__tablet-guarantee-period-text">
+                      Guarantee Period:
+                    </span>
+                    <span className="my-candidates-candidate-details-page__tablet-guarantee-period-duration">
+                      30 days
+                    </span>
+                  </p>
+                  <p className="my-candidates-candidate-details-page__tablet-fee-agreement-text">
+                    Fee Agreement
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="my-candidates-candidate-details-page__tablet-education-container">
+              <p className="my-candidates-candidate-details-page__tablet-education-header">
+                Education
+              </p>
+              <p className="my-candidates-candidate-details-page__tablet-education-description">
+                Associate Degree in Mechanical Drafting and Design Technology,
+                Milwaukee Area Technical College, 2014
+              </p>
+            </div>
+            <div className="my-candidates-candidate-details-page__tablet-required-skills-container">
+              <p className="my-candidates-candidate-details-page__tablet-required-skills-header">
+                Required Skills
+              </p>
+              <p className="my-candidates-candidate-details-page__tablet-required-skills-description">
+                AutoCAD proficiency, 3D modeling & simulation, bulk material
+                handling systems, mechanical drafting & design, tolerance
+                analysis, project management, technical documentation,
+                cross-functional team collaboration.
+              </p>
+            </div>
+            <div className="my-candidates-candidate-details-page__tablet-other-requirements-container">
+              <p className="my-candidates-candidate-details-page__tablet-other-requirements-header">
+                Other Requirements
+              </p>
+              <p className="my-candidates-candidate-details-page__tablet-other-requirements-description">
+                Resident of San Antonio, Texas
+              </p>
+            </div>
+            <div className="my-candidates-candidate-details-page__tablet-orange-background-section"></div>
+            <div className="my-candidates-candidate-details-page__tablet-bottom-button-container">
+              <button className="my-candidates-candidate-details-page__tablet-post-candidate-button">
+                Post Candidate
+              </button>
+              <button className="my-candidates-candidate-details-page__tablet-edit-button">
+                Edit
+              </button>
+            </div>
           </div>
         </div>
       </div>
