b3b2fcdc9cfd8f23a1b8120369a7578c85682907
aent <alintssr@gmail.com>
2024-11-17 02:34:55 -0500

    latest code

--- src/pages/PartnershipsViewAgreementPage/PartnershipsViewAgreementPage.js
+++ src/pages/PartnershipsViewAgreementPage/PartnershipsViewAgreementPage.js
@@ -59,8 +59,266 @@ export default function PartnershipsViewAgreementPage() {
                       alt="Rating Star Icon"
                     />
                   </div>
+                  <p className="partnerships-view-agreement-page__mobile-first-row-reviews-text">
+                    5
+                  </p>
                 </div>
               </div>
+              <div className="partnerships-view-agreement-page__mobile-first-row-active-jobs-active-candidates-container">
+                <div className="partnerships-view-agreement-page__mobile-first-row-active-jobs-container">
+                  <p className="partnerships-view-agreement-page__mobile-first-row-active-jobs-text">
+                    Active Jobs
+                  </p>
+                  <p className="partnerships-view-agreement-page__mobile-first-row-active-jobs-number">
+                    <span className="partnerships-view-agreement-page__mobile-first-row-active-jobs-first-number">
+                      2
+                    </span>
+                    <span className="partnerships-view-agreement-page__mobile-first-row-active-jobs-second-number">
+                      /17
+                    </span>
+                  </p>
+                </div>
+                <div className="partnerships-view-agreement-page__mobile-first-row-active-candidates-container">
+                  <p className="partnerships-view-agreement-page__mobile-first-row-active-candidates-text">
+                    Active Candidates
+                  </p>
+                  <p className="partnerships-view-agreement-page__mobile-first-row-active-candidates-number">
+                    <span className="partnerships-view-agreement-page__mobile-first-row-active-candidates-first-number">
+                      1
+                    </span>
+                    <span className="partnerships-view-agreement-page__mobile-first-row-active-candidates-second-number">
+                      /4
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="partnerships-view-agreement-page__mobile-content-container">
+              <p className="partnerships-view-agreement-page__mobile-instructions-for-signing-the-partnership-agreement-header">
+                Instructions for Signing the Partnership Agreement
+              </p>
+              <p className="partnerships-view-agreement-page__mobile-before-you-proceed">
+                Before you proceed with signing the Partnership Agreement,
+                please take the following steps to ensure everything is
+                completed correctly.
+              </p>
+              <p className="partnerships-view-agreement-page__mobile-read-carefully">
+                1.
+                <span className="partnerships-view-agreement-page__mobile-bolded-text">
+                  **Read Carefully.**
+                </span>{" "}
+                Review the entire Agreement and Terms and Conditions thoroughly.
+                Make sure you fully understand each section, including the
+                responsibilities, profit-sharing terms, confidentiality
+                requirements, and termination conditions.
+              </p>
+              <p className="partnerships-view-agreement-page__mobile-seek-clarification">
+                2.{" "}
+                <span className="partnerships-view-agreement-page__mobile-bolded-text">
+                  **Seek Clarification.**
+                </span>{" "}
+                If there is any part of the Agreement that is unclear or if you
+                have any questions, please consult with your legal advisor or
+                contact the other Party to discuss and clarify any points before
+                signing.
+              </p>
+              <p className="partnerships-view-agreement-page__mobile-verify-details">
+                3.{" "}
+                <span className="partnerships-view-agreement-page__mobile-bolded-text">
+                  **Verify Details.**
+                </span>{" "}
+                Double check that all the information, such as your name, title,
+                and the date, is accurate and correctly filled out in the
+                designated sections.
+              </p>
+              <p className="partnerships-view-agreement-page__mobile-signature">
+                4.
+                <span className="partnerships-view-agreement-page__mobile-bolded-text">
+                  **Signature.**
+                </span>{" "}
+                When you are ready to sign, please do so in the space provided
+                below your name and title. Your signature should be clear and
+                legible.
+              </p>
+              <p className="partnerships-view-agreement-page__mobile-date">
+                5.
+                <span className="partnerships-view-agreement-page__mobile-bolded-text">
+                  **Date.**
+                </span>{" "}
+                Ensure you include the date of signing next to your signature.
+                This date should match the day you actually sign the document.
+              </p>
+              <p className="partnerships-view-agreement-page__desktop-by-following-these-instructions">
+                By following these instructions, you help ensure the Agreement
+                is executed properly, making it a valid and binding document for
+                all Parties involved.
+              </p>
+              <p className="partnerships-view-agreement-page__desktop-partnership-agreement-header">
+                Partnership Agreement
+              </p>
+              <p className="partnerships-view-agreement-page__desktop-partnership-agreement-description">
+                This Partnership Agreement ("Agreement") is made and entered
+                into as of [Date], by and between [Agency 1 Name], a
+                [State][Type of Business Entity] with its principal office
+                located at [Agency 1 Address], and [Agency 2 Name], a
+                [State][Type of Business Entity] with its principal office
+                located at [Agency 2 Address] (collectively referred to as the
+                "Parties" and individually as a "Party").
+              </p>
+              <div className="partnerships-view-agreement-page__desktop-purpose-container">
+                <p className="partnerships-view-agreement-page__desktop-purpose-header">
+                  1.{" "}
+                  <span className="partnerships-view-agreement-page__desktop-extra-space">
+                    Purpose
+                  </span>
+                </p>
+                <p className="partnerships-view-agreement-page__desktop-purpose-description">
+                  The purpose of this Agreement is to establish a partnership
+                  between the Parties for the mutual benefit of both agencies.
+                  The Parties agree to collaborate and share resources,
+                  expertise, and information to enhance their service offerings
+                  and achieve common business objectives.
+                </p>
+              </div>
+              <div className="partnerships-view-agreement-page__desktop-responsibilities-of-the-parties-container">
+                <p className="partnerships-view-agreement-page__desktop-responsibilities-of-the-parties-header">
+                  2.{" "}
+                  <span className="partnerships-view-agreement-page__desktop-extra-space">
+                    Responsibilities of the Parties
+                  </span>
+                </p>
+                <p className="partnerships-view-agreement-page__desktop-responsibilities-of-the-parties-description-agency-1">
+                  -**[Agency 1 Name].** [Agency 1 Name] agrees to provide
+                  [specific services, resources, or contributions] as part of
+                  this partnership.
+                </p>
+                <p className="partnerships-view-agreement-page__desktop-responsibilities-of-the-parties-description-agency-2">
+                  -**[Agency 2 Name].** [Agency 2 Name] agrees to provide
+                  [specific services, resources, or contributions] as part of
+                  this partnership.
+                </p>
+              </div>
+              <div className="partnerships-view-agreement-page__desktop-term-container">
+                <p className="partnerships-view-agreement-page__desktop-term-header">
+                  3. <span className="cccc">Term</span>
+                </p>
+                <p className="partnerships-view-agreement-page__desktop-term-description">
+                  This Agreement shall commence on the date of execution and
+                  shall continue in effect until terminated by either Party in
+                  accordance with the terms of this Agreement.
+                </p>
+              </div>
+              <div className="partnerships-view-agreement-page__desktop-confidentiality-container">
+                <p className="partnerships-view-agreement-page__desktop-confidentiality-header">
+                  4.{" "}
+                  <span className="partnerships-view-agreement-page__desktop-extra-space">
+                    Confidentiality
+                  </span>
+                </p>
+                <p className="partnerships-view-agreement-page__desktop-confidentiality-description">
+                  The Parties agree to maintain the confidentiality of any
+                  proprietary or confidential information disclosed during the
+                  course of this partnership. Neither party shall use or
+                  disclose such information for any purpose other than as
+                  necessary to fulfill the obligations under this Agreement.
+                </p>
+              </div>
+              <div className="partnerships-view-agreement-page__desktop-profit-sharing-container">
+                <p className="partnerships-view-agreement-page__desktop-profit-sharing-header">
+                  5.{" "}
+                  <span className="partnerships-view-agreement-page__desktop-extra-space">
+                    Profit Sharing
+                  </span>
+                </p>
+                <p className="partnerships-view-agreement-page__desktop-profit-sharing-description">
+                  The Parties agree to share any profits or revenue generated as
+                  a result of this partnership in the following manner: [Specify
+                  profit-sharing ratio or terms]. Payments shall be made on a
+                  [monthly/quarterly] basis, with detailed accounting provided
+                  to each Party.
+                </p>
+              </div>
+              <div className="partnerships-view-agreement-page__desktop-termination-container">
+                <p className="partnerships-view-agreement-page__desktop-termination-header">
+                  6.{" "}
+                  <span className="partnerships-view-agreement-page__desktop-extra-space">
+                    Termination
+                  </span>
+                </p>
+                <p className="partnerships-view-agreement-page__desktop-termination-description">
+                  Either Party may terminate this Agreement at any time by
+                  providing [number] days' written notice to the other Party.
+                  Upon termination, the Parties shall cease all joint activities
+                  and settle any outstanding financial obligations.
+                </p>
+              </div>
+              <div className="partnerships-view-agreement-page__desktop-governing-law-container">
+                <p className="partnerships-view-agreement-page__desktop-governing-law-header">
+                  7.{" "}
+                  <span className="partnerships-view-agreement-page__desktop-extra-space">
+                    Governing Law
+                  </span>
+                </p>
+                <p className="partnerships-view-agreement-page__desktop-governing-law-description">
+                  This Agreement shall be governed by and construed in
+                  accordance with the laws of the State of [State], without
+                  regard to its conflict of laws principles.
+                </p>
+              </div>
+              <div className="partnerships-view-agreement-page__desktop-entire-agreement-container">
+                <p className="partnerships-view-agreement-page__desktop-entire-agreement-header">
+                  8.{" "}
+                  <span className="partnerships-view-agreement-page__desktop-extra-space">
+                    Entire Agreement
+                  </span>
+                </p>
+                <p className="partnerships-view-agreement-page__desktop-entire-agreement-description">
+                  This Agreement constitutes the entire understanding between
+                  the Parties with respect to the subject matter hereof and
+                  supersedes all prior discussions, agreements, or
+                  understandings of any kind.
+                </p>
+              </div>
+              <div className="partnerships-view-agreement-page__desktop-amendments-container">
+                <p className="partnerships-view-agreement-page__desktop-amendments-header">
+                  9.{" "}
+                  <span className="partnerships-view-agreement-page__desktop-extra-space">
+                    Amendments
+                  </span>
+                </p>
+                <p className="partnerships-view-agreement-page__desktop-amendments-description">
+                  This Agreement may be amended or modified only in writing,
+                  signed by both Parties.
+                </p>
+              </div>
+              <div className="partnerships-view-agreement-page__desktop-miscellaneous-container">
+                <p className="partnerships-view-agreement-page__desktop-miscellaneous-header">
+                  10.{" "}
+                  <span className="partnerships-view-agreement-page__desktop-extra-space">
+                    Miscellaneous
+                  </span>
+                </p>
+                <p className="partnerships-view-agreement-page__desktop-miscellaneous-description-line-1">
+                  -**Dispute Resolution.** Any disputes arising out of or in
+                  connection with this Agreement shall be resolved through
+                  [mediation/arbitration] in [Location].
+                </p>
+                <p className="partnerships-view-agreement-page__desktop-miscellaneous-description-line-2">
+                  -**Severability.** If any provision of this Agreement is found
+                  to be invalid or unenforceable, the remainder of the Agreement
+                  shall continue in full force and effect.
+                </p>
+              </div>
+              <p className="partnerships-view-agreement-page__desktop-terms-and-conditions-for-accepting-to-partner-with-header">
+                Terms and Conditions for Accepting to Partner with Staffing
+                Agency
+              </p>
+              <p className="partnerships-view-agreement-page__desktop-terms-and-conditions-for-accepting-to-partner-with-description">
+                Please carefully read and understand these terms and conditions
+                before requesting client details. By clicking on the
+                "Acknowledge & Agree" button, you acknowledge and agree to the
+                following terms and conditions:
+              </p>
             </div>
           </div>
         </div>
