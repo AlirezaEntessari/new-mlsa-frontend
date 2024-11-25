07969d9070ed29c28c5a613639ce8e7a53ec24e8
Alireza Entessari <alintssr@gmail.com>
2024-11-25 17:38:26 -0500

    Latest code

--- src/pages/HelpMyAccountPage/HelpMyAccountPage.js
+++ src/pages/HelpMyAccountPage/HelpMyAccountPage.js
@@ -309,6 +309,181 @@ export default function HelpMyAccountPage() {
                       Set Permissions
                     </span>
                   </p>
+                  <p className="help-my-account-page__tablet-notification-settings-option">
+                    <img
+                      className="help-my-account-page__tablet-paper-icon"
+                      src={PaperIcon}
+                      alt="Paper Icon"
+                    />
+                    <span className="help-my-account-page__tablet-notification-settings-text">
+                      Notification Settings
+                    </span>
+                  </p>
+                  <p className="help-my-account-page__tablet-vacation-mode-option">
+                    <img
+                      className="help-my-account-page__tablet-paper-icon"
+                      src={PaperIcon}
+                      alt="Paper Icon"
+                    />
+                    <span className="help-my-account-page__tablet-vacation-mode-text">
+                      Vacation Mode
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="help-my-account-page__desktop-container">
+            <p className="help-my-account-page__desktop-help-center-header">
+              Help Center
+            </p>
+            <div className="help-my-account-page__desktop-central-container">
+              <div className="help-my-account-page__desktop-left-container">
+                <img
+                  className="help-my-account-page__desktop-search-icon"
+                  src={SearchIcon}
+                  alt="Search Icon"
+                />
+                <input
+                  className="help-my-account-page__desktop-im-looking-for-input"
+                  type="text"
+                />
+                <div className="help-my-account-page__desktop-categories-container">
+                  <p className="help-my-account-page__desktop-categories-header">
+                    Categories
+                  </p>
+                  <p className="help-my-account-page__desktop-my-account-option">
+                    My Account
+                  </p>
+                  <p className="help-my-account-page__desktop-job-posting-option">
+                    Job Posting
+                  </p>
+                  <p className="help-my-account-page__desktop-candidate-posting-option">
+                    Candidate Posting
+                  </p>
+                  <p className="help-my-account-page__desktop-documents-option">
+                    Documents
+                  </p>
+                  <p className="help-my-account-page__desktop-partnerships-option">
+                    Partnerships
+                  </p>
+                  <p className="help-my-account-page__desktop-dashboard-option">
+                    Dashboard
+                  </p>
+                  <p className="help-my-account-page__desktop-searching-option">
+                    Searching
+                  </p>
+                  <p className="help-my-account-page__desktop-ratings-and-feedback-option">
+                    Ratings & Feedback
+                  </p>
+                  <p className="help-my-account-page__desktop-reports-option">
+                    Reports
+                  </p>
+                  <p className="help-my-account-page__desktop-membership-option">
+                    Membership
+                  </p>
+                  <p className="help-my-account-page__desktop-policies-and-terms-option">
+                    Policies & Terms
+                  </p>
+                  <p className="help-my-account-page__desktop-faq-option">
+                    FAQ
+                  </p>
+                </div>
+              </div>
+              <div className="help-my-account-page__desktop-right-container">
+                <div className="help-my-account-page__desktop-my-account-container">
+                  <div className="help-my-account-page__desktop-my-account-header-container">
+                    <p className="help-my-account-page__desktop-my-account-header">
+                      My Account
+                    </p>
+                    <img
+                      className="help-my-account-page__desktop-my-account-icon"
+                      src={MyAccountIcon}
+                      alt="My Account Icon"
+                    />
+                  </div>
+                  <div className="help-my-account-page__desktop-my-account-options-container">
+                    <p className="help-my-account-page__desktop-my-account-overview-option">
+                      <img
+                        className="help-my-account-page__desktop-paper-icon"
+                        src={PaperIcon}
+                        alt="Paper Icon"
+                      />
+                      <span className="help-my-account-page__desktop-my-account-overview-text">
+                        My Account Overview
+                      </span>
+                    </p>
+                    <p className="help-my-account-page__desktop-change-password-option">
+                      <img
+                        className="help-my-account-page__desktop-paper-icon"
+                        src={PaperIcon}
+                        alt="Paper Icon"
+                      />
+                      <span className="help-my-account-page__desktop-change-password-text">
+                        Change Password
+                      </span>
+                    </p>
+                    <p className="help-my-account-page__desktop-view-and-edit-profile-option">
+                      <img
+                        className="help-my-account-page__desktop-paper-icon"
+                        src={PaperIcon}
+                        alt="Paper Icon"
+                      />
+                      <span className="help-my-account-page__desktop-view-and-edit-profile-text">
+                        View and Edit Profile
+                      </span>
+                    </p>
+                    <p className="help-my-account-page__desktop-view-and-edit-agency-information-option">
+                      <img
+                        className="help-my-account-page__desktop-paper-icon"
+                        src={PaperIcon}
+                        alt="Paper Icon"
+                      />
+                      <span className="help-my-account-page__desktop-view-and-edit-agency-information-text">
+                        View and Edit Agency Information
+                      </span>
+                    </p>
+                    <p className="help-my-account-page__desktop-manage-users-option">
+                      <img
+                        className="help-my-account-page__desktop-paper-icon"
+                        src={PaperIcon}
+                        alt="Paper Icon"
+                      />
+                      <span className="help-my-account-page__desktop-manage-users-text">
+                        Manage Users
+                      </span>
+                    </p>
+                    <p className="help-my-account-page__desktop-set-permissions-option">
+                      <img
+                        className="help-my-account-page__desktop-paper-icon"
+                        src={PaperIcon}
+                        alt="Paper Icon"
+                      />
+                      <span className="help-my-account-page__desktop-set-permissions-text">
+                        Set Permissions
+                      </span>
+                    </p>
+                    <p className="help-my-account-page__desktop-notification-settings-option">
+                      <img
+                        className="help-my-account-page__desktop-paper-icon"
+                        src={PaperIcon}
+                        alt="Paper Icon"
+                      />
+                      <span className="help-my-account-page__desktop-notification-settings-text">
+                        Notification Settings
+                      </span>
+                    </p>
+                    <p className="help-my-account-page__desktop-vacation-mode-option">
+                      <img
+                        className="help-my-account-page__desktop-paper-icon"
+                        src={PaperIcon}
+                        alt="Paper Icon"
+                      />
+                      <span className="help-my-account-page__desktop-vacation-mode-text">
+                        Vacation Mode
+                      </span>
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
