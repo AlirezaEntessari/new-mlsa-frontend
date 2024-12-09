72e7ef6e6d65d90b3e066c6cc12d7271c3472ce7
aent <alintssr@gmail.com>
2024-12-08 23:33:19 -0500

    Latest code

--- src/pages/RatingsHomePage/RatingsHomePage.js
+++ src/pages/RatingsHomePage/RatingsHomePage.js
@@ -294,7 +294,9 @@ export default function RatingsHomePage() {
                       <p className="ratings-home-page__tablet-about-ratings-professionalism">
                         Professionalism
                       </p>
-                      <p className="ratings-home-page__tablet-about-ratings-payment-timeliness">Payment Timeliness</p>
+                      <p className="ratings-home-page__tablet-about-ratings-payment-timeliness">
+                        Payment Timeliness
+                      </p>
                       <p className="ratings-home-page__tablet-about-ratings-resource-quality">
                         Resource Quality
                       </p>
@@ -306,8 +308,125 @@ export default function RatingsHomePage() {
                     />
                   </div>
                 </div>
+                <div className="ratings-home-page__tablet-feedback-container">
+                  <p className="ratings-home-page__tablet-feedback-header">
+                    Feedback
+                  </p>
+                  <p className="ratings-home-page__tablet-feedback-description">
+                    The feedback you receive reflects your ability to perform an
+                    honest and transparent transaction. If you disagree with any
+                    feedback you receive, you may challenge it by disputing it.
+                  </p>
+                </div>
               </div>
             </div>
+            <div className="ratings-home-page__tablet-giving-feedback-container">
+              <div className="ratings-home-page__tablet-giving-feedback-inner-container">
+                <p className="ratings-home-page__tablet-giving-feedback-header">
+                  Giving Feedback
+                </p>
+                <p className="ratings-home-page__tablet-giving-feedback-description">
+                  When leaving feedback for an Agency, it's important to
+                  <span className="ratings-home-page__tablet-bolded-text">
+                    follow proper etiquette
+                  </span>
+                  to ensure it is both
+                  <span className="ratings-home-page__tablet-bolded-text">
+                    constructive and fair.
+                  </span>
+                  If you're pleased with your experience, a
+                  <span className="ratings-home-page__tablet-bolded-text">
+                    high-scoring rating is simple to provide
+                  </span>
+                  and greatly appreciated. However, if your experience was less
+                  than satisfactory, we encourage you to first contact the
+                  Agency and discuss this. If this action does not produce
+                  positive results, detail your concerns in your feedback. When
+                  you are giving feedback that is less than satisfactory, you
+                  will be asked by MLSA to answer specific questions to help us
+                  understand the issues you experienced. This process allows us
+                  to thoroughly investigate any problems and take corrective
+                  actions. Your thoughtful feedback is crucial for
+                  <span className="ratings-home-page__tablet-bolded-text">
+                    our ongoing improvement and commitment to excellence.
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div className="ratings-home-page__tablet-ratings-container">
+              <p className="ratings-home-page__tablet-ratings-header">
+                Ratings
+              </p>
+              <p className="ratings-home-page__tablet-ratings-description">
+                Ratings are calculated by averaging scores across four key
+                categories:
+                <span className="ratings-home-page__tablet-bolded-text">
+                  communication, professionalism, payment timeliness, and
+                  resource quality.
+                </span>
+                Each of these categories reflects an important aspect of the
+                service provided.
+                <span className="ratings-home-page__tablet-bolded-text">
+                  Communication
+                </span>
+                measures how effectively and clearly information is conveyed,
+                while
+                <span className="ratings-home-page__tablet-bolded-text">
+                  professionalism
+                </span>
+                assesses the courtesy, respect, and competence shown throughout
+                the interaction.
+                <span className="ratings-home-page__tablet-bolded-text">
+                  Payment Timeliness
+                </span>
+                captures the promptness and reliability of the service, and
+                <span className="ratings-home-page__tablet-bolded-text">
+                  Resource Quality
+                </span>
+                refers to the overall standard, suitability, and effectiveness
+                of both an Agency's candidates and job roles. By averaging these
+                scores, the rating provides a comprehensive measure of
+                performance, helping to
+                <span className="ratings-home-page__tablet-bolded-text">
+                  ensure consistent quality and continuous improvement.
+                </span>
+              </p>
+            </div>
+            <img
+              className="ratings-home-page__tablet-girl-with-laptop-stars-icon"
+              src={GirlWithLaptopStarsIcon}
+              alt="Girl With Laptop Stars Icon"
+            />
+            <div className="ratings-home-page__tablet-bottom-container">
+              <div className="ratings-home-page__tablet-bottom-inner-container">
+                <p className="ratings-home-page__tablet-bottom-inner-container-description">
+                  Ratings are at the core of the MLSA platform. They help
+                  maintain and
+                  <span className="ratings-home-page__tablet-bolded-text">
+                    elevate our standards of service,
+                  </span>
+                  ensuring that only the best rise to the top. High ratings
+                  highlight exceptional performance, allowing those who
+                  consistently deliver outstanding service to be
+                  <span className="ratings-home-page__tablet-bolded-text">
+                    recognized and rewarded.
+                  </span>
+                  At the same time, constructive feedback identifies areas for
+                  improvement, helping us to address any issues and
+                  <span className="ratings-home-page__tablet-bolded-text">
+                    enhance the overall member experience.
+                  </span>
+                  This process not only keeps Agencies accountable but also
+                  ensures that our top performers are celebrated, driving
+                  <span className="ratings-home-page__tablet-bolded-text">
+                    all MLSA Members to consistently strive for excellence.
+                  </span>
+                  Your ratings are a key part of this ongoing pursuit,
+                  benefiting both MLSA and our Members.
+                </p>
+              </div>
+            </div>
+            <p className="ratings-home-page__tablet-learn-more">Learn more about Ratings and Feedback</p>
           </div>
         </div>
       </div>
