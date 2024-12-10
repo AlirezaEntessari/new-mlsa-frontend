c965f11716c2187ce0e076221fade60d86dbe9d3
Alireza Entessari <alintssr@gmail.com>
2024-12-10 14:29:22 -0500

    Latest code

--- src/pages/RatingsRateAnAgencyPage/RatingsRateAnAgencyPage.js
+++ src/pages/RatingsRateAnAgencyPage/RatingsRateAnAgencyPage.js
@@ -122,6 +122,209 @@ export default function RatingsRateAnAgencyPage() {
               </button>
             </div>
           </div>
+          <div className="ratings-rate-an-agency-page__tablet-container">
+            <p className="ratings-rate-an-agency-page__tablet-header">
+              Rate an Agency
+            </p>
+            <img
+              className="ratings-rate-an-agency-page__tablet-values-circle-graphic-icon"
+              src={ValuesCircleGraphicIcon}
+              alt="Values Circle Graphic Icon"
+            />
+            <div className="ratings-rate-an-agency-page__tablet-input-container">
+              <img
+                className="ratings-rate-an-agency-page__tablet-search-icon"
+                src={SearchIcon}
+                alt="Search Icon"
+              />
+              <input
+                className="ratings-rate-an-agency-page__tablet-enter-agency-name-input"
+                type="text"
+                placeholder="Enter Agency Name"
+              />
+            </div>
+            <div className="ratings-rate-an-agency-page__tablet-content-container">
+              <div className="ratings-rate-an-agency-page__tablet-communication-container">
+                <p className="ratings-rate-an-agency-page__tablet-communication-header">
+                  Communication
+                </p>
+                <p className="ratings-rate-an-agency-page__tablet-communication-description">
+                  Evaluate the effectiveness and clarity of communication with
+                  the agency.
+                </p>
+                <img
+                  className="ratings-rate-an-agency-page__tablet-ratings-empty-stars-icon"
+                  src={RatingsEmptyStarsIcon}
+                  alt="Ratings Empty Stars Icon"
+                />
+              </div>
+              <div className="ratings-rate-an-agency-page__tablet-professionalism-container">
+                <p className="ratings-rate-an-agency-page__tablet-professionalism-header">
+                  Professionalism
+                </p>
+                <p className="ratings-rate-an-agency-page__tablet-professionalism-description">
+                  Assess the agency's conduct and adherence to industry
+                  standards.
+                </p>
+                <img
+                  className="ratings-rate-an-agency-page__tablet-ratings-empty-stars-icon"
+                  src={RatingsEmptyStarsIcon}
+                  alt="Ratings Empty Stars Icon"
+                />
+              </div>
+              <div className="ratings-rate-an-agency-page__tablet-payment-timeliness-container">
+                <p className="ratings-rate-an-agency-page__tablet-payment-timeliness-header">
+                  Payment Timeliness
+                </p>
+                <p className="ratings-rate-an-agency-page__tablet-payment-timeliness-description">
+                  Rate the agency's speed in handling payments.
+                </p>
+                <img
+                  className="ratings-rate-an-agency-page__tablet-ratings-empty-stars-icon"
+                  src={RatingsEmptyStarsIcon}
+                  alt="Ratings Empty Stars Icon"
+                />
+              </div>
+              <div className="ratings-rate-an-agency-page__tablet-resource-quality-container">
+                <p className="ratings-rate-an-agency-page__tablet-resource-quality-header">
+                  Resource Quality
+                </p>
+                <p className="ratings-rate-an-agency-page__tablet-resource-quality-description">
+                  Evaluate the agency's overall standard, suitability, and
+                  effectiveness of both the candidates and the job roles.
+                </p>
+                <img
+                  className="ratings-rate-an-agency-page__tablet-ratings-empty-stars-icon"
+                  src={RatingsEmptyStarsIcon}
+                  alt="Ratings Empty Stars Icon"
+                />
+              </div>
+              <div className="ratings-rate-an-agency-page__tablet-feedback-container">
+                <p className="ratings-rate-an-agency-page__tablet-feedback-header">
+                  Feedback:
+                </p>
+                <p className="ratings-rate-an-agency-page__tablet-feedback-description">
+                  Provide comments or feedback on your experience with the
+                  agency.
+                </p>
+              </div>
+            </div>
+            <textarea
+              className="ratings-rate-an-agency-page__tablet-text-area"
+              name="tablet-text-area"
+              id="tablet-text-area"
+            ></textarea>
+            <div className="ratings-rate-an-agency-page__tablet-bottom-button-container">
+              <button className="ratings-rate-an-agency-page__tablet-submit-button">
+                Submit
+              </button>
+              <button className="ratings-rate-an-agency-page__tablet-cancel-button">
+                Cancel
+              </button>
+            </div>
+          </div>
+          <div className="ratings-rate-an-agency-page__desktop-container">
+            <p className="ratings-rate-an-agency-page__desktop-header">
+              Rate an Agency
+            </p>
+            <div className="ratings-rate-an-agency-page__desktop-content-container">
+              <div className="ratings-rate-an-agency-page__desktop-content-left-container">
+                <div className="ratings-rate-an-agency-page__desktop-enter-agency-name-container">
+                  <img
+                    className="ratings-rate-an-agency-page__desktop-search-icon"
+                    src={SearchIcon}
+                    alt="Search Icon"
+                  />
+                  <input
+                    className="ratings-rate-an-agency-page__desktop-enter-agency-name-input"
+                    type="text"
+                  />
+                </div>
+                <div className="ratings-rate-an-agency-page__desktop-communication-container">
+                  <p className="ratings-rate-an-agency-page__desktop-communication-header">
+                    Communication
+                  </p>
+                  <p className="ratings-rate-an-agency-page__desktop-communication-description">
+                    Evaluate the effectiveness and clarity of communication with
+                    the agency.
+                  </p>
+                  <img
+                    className="ratings-rate-an-agency-page__desktop-ratings-empty-stars-icon"
+                    src={RatingsEmptyStarsIcon}
+                    alt="Ratings Empty Stars Icon"
+                  />
+                </div>
+                <div className="ratings-rate-an-agency-page__desktop-professionalism-container">
+                  <p className="ratings-rate-an-agency-page__desktop-professionalism-header">
+                    Professionalism
+                  </p>
+                  <p className="ratings-rate-an-agency-page__desktop-professionalism-description">
+                    Assess the agency's conduct and adherence to industry
+                    standards.
+                  </p>
+                  <img
+                    className="ratings-rate-an-agency-page__desktop-ratings-empty-stars-icon"
+                    src={RatingsEmptyStarsIcon}
+                    alt="Ratings Empty Stars Icon"
+                  />
+                </div>
+                <div className="ratings-rate-an-agency-page__desktop-payment-timeliness-container">
+                  <p className="ratings-rate-an-agency-page__desktop-payment-timeliness-header">
+                    Payment Timeliness
+                  </p>
+                  <p className="ratings-rate-an-agency-page__desktop-payment-timeliness-description">
+                    Rate the agency's speed in handling payments.
+                  </p>
+                  <img
+                    className="ratings-rate-an-agency-page__desktop-ratings-empty-stars-icon"
+                    src={RatingsEmptyStarsIcon}
+                    alt="Ratings Empty Stars Icon"
+                  />
+                </div>
+                <div className="ratings-rate-an-agency-page__desktop-resource-quality-container">
+                  <p className="ratings-rate-an-agency-page__desktop-resource-quality-header">
+                    Resource Quality
+                  </p>
+                  <p className="ratings-rate-an-agency-page__desktop-resource-quality-description">
+                    Evaluate the agency's overall standard, suitability, and
+                    effectiveness of both the candidates and job roles.
+                  </p>
+                  <img
+                    className="ratings-rate-an-agency-page__desktop-ratings-empty-stars-icon"
+                    src={RatingsEmptyStarsIcon}
+                    alt="Ratings Empty Stars Icon"
+                  />
+                </div>
+                <div className="ratings-rate-an-agency-page__desktop-feedback-container">
+                  <p className="ratings-rate-an-agency-page__desktop-feedback-header">
+                    Feedback:
+                  </p>
+                  <p className="ratings-rate-an-agency-page__desktop-feedback-description">
+                    Provide comments or feedback on your experience with the
+                    agency.
+                  </p>
+                </div>
+              </div>
+              <img
+                className="ratings-rate-an-agency-page__desktop-values-circle-graphic-icon"
+                src={ValuesCircleGraphicIcon}
+                alt="Values Circle Graphic Icon"
+              />
+            </div>
+            <textarea
+              className="ratings-rate-an-agency-page__desktop-text-area"
+              name="desktop-text-area"
+              id="desktop-text-area"
+            ></textarea>
+            <div className="ratings-rate-an-agency-page__desktop-bottom-button-container">
+              <button className="ratings-rate-an-agency-page__desktop-submit-button">
+                Submit
+              </button>
+              <button className="ratings-rate-an-agency-page__desktop-cancel-button">
+                Cancel
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
