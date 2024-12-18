0c563eb34fb22e1a144e2679c243070e5926b607
Alireza Entessari <alintssr@gmail.com>
2024-12-18 17:28:10 -0500

    Added conflict resolution page

--- src/pages/RatingsConflictResolutionPage/RatingsConflictResolutionPage.js
+++ src/pages/RatingsConflictResolutionPage/RatingsConflictResolutionPage.js
@@ -152,8 +152,198 @@ export default function RatingsConflictResolutionPage() {
               <p className="ratings-conflict-resolution-page__mobile-guidelines-for-rating-agencies-and-writing-feedback-description">
                 When submitting a rating or feedback, it's important to ensure
                 that your review is fair, accurate, and constructive. Below are
-                guidelines to help you provide meaningful and effective ratings.
+                guidelines to help you provide meaningful and effective ratings:
               </p>
+              <ol className="ratings-conflict-resolution-page__mobile-guidelines-for-rating-agencies-and-writing-feedback-list">
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-rating-agencies-and-writing-feedback-list-first-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Be Honest and Transparent
+                  </span>
+                  - Always provide a truthful account of your experience.
+                  Ratings should reflect the actual quality of the service or
+                  interaction.
+                </li>
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-rating-agencies-and-writing-feedback-list-second-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Focus on Specifics
+                  </span>
+                  - Highlight key details that led to your rating. Provide clear
+                  examples to support your feedback, whether positive or
+                  negative.
+                </li>
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-rating-agencies-and-writing-feedback-list-third-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Be Respectful and Professional
+                  </span>
+                  - Use courteous language, even when discussing areas of
+                  improvement. Avoid personal attacks or emotional outbursts.
+                </li>
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-rating-agencies-and-writing-feedback-list-fourth-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Consider Resolution First
+                  </span>
+                  - Before submitting a negative rating (anything below 4
+                  stars), consider resolving the issue directly with the other
+                  party through clear communication.
+                </li>
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-rating-agencies-and-writing-feedback-list-fifth-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Use Constructive Criticism
+                  </span>
+                  - If you provide lower ratings, offer suggestions for
+                  improvement. This can help the other party understand how to
+                  improve their performance.
+                </li>
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-rating-agencies-and-writing-feedback-list-sixth-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Avoid Bias
+                  </span>
+                  - Focus on the facts of the experience, not personal feelings
+                  or external factors unrelated to the service provided.
+                </li>
+              </ol>
+              <p className="ratings-conflict-resolution-page__mobile-guidelines-for-rating-agencies-and-writing-feedback-bottom-description">
+                By following these guidelines, you help maintain a balanced and
+                professional review system that benefits all users.
+              </p>
+            </div>
+            <div className="ratings-conflict-resolution-page__mobile-guidelines-for-receiving-negative-ratings-or-feedback-container">
+              <p className="ratings-conflict-resolution-page__mobile-guidelines-for-receiving-negative-ratings-or-feedback-header">
+                Guidelines for Receiving Negative Ratings or Feedback
+              </p>
+              <p className="ratings-conflict-resolution-page__mobile-guidelines-for-receiving-negative-ratings-or-feedback-description">
+                Receiving negative feedback can be challenging, but it's
+                important to handle the situation professionally. Below are
+                guidelines to help you respond constructively and resolve
+                issues:
+              </p>
+              <ol className="ratings-conflict-resolution-page__mobile-guidelines-for-receiving-negative-ratings-or-feedback-list">
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-receiving-negative-ratings-or-feedback-first-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Stay Calm and Professional
+                  </span>
+                  - Avoid reacting emotionally. Take time to process the
+                  feedback before responding to ensure a calm and thoughtful
+                  approach. Waiting 24 hours before responding is recommended.
+                </li>
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-receiving-negative-ratings-or-feedback-second-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Review the Feedback Carefully
+                  </span>
+                  - Understand the specific concerns or issues raised. Focus on
+                  the details to identify where improvements can be made. If you
+                  find yourself becoming emotional, step away and reread the
+                  feedback a second or third time with a fresh look.
+                </li>
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-receiving-negative-ratings-or-feedback-third-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Reach Out for Clarification
+                  </span>
+                  - if any part of the feedback seems unclear to unfair, contact
+                  the other party to discuss it professionally. Open
+                  communication can often resolve misunderstandings.
+                </li>
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-receiving-negative-ratings-or-feedback-fourth-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Acknowledge Mistakes
+                  </span>
+                  - If valid points are raised, acknowledge them and explain how
+                  you plan to address the issues in the future. This shows
+                  responsibility and a willingness to improve.
+                </li>
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-receiving-negative-ratings-or-feedback-fifth-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Request a Review Update
+                  </span>
+                  - If you've taken steps to resolve the issue, politely ask the
+                  other party if they are willing to update their rating or
+                  feedback to reflect the improved situation.
+                </li>
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-receiving-negative-ratings-or-feedback-sixth-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Learn and Grow
+                  </span>
+                  - Use the feedback as an opportunity to learn and improve.
+                  Constructive criticism can help you refine your services or
+                  approach.
+                </li>
+                <li className="ratings-conflict-resolution-page__mobile-guidelines-for-receiving-negative-ratings-or-feedback-seventh-step">
+                  <span className="ratings-conflict-resolution-page__mobile-guidelines-bolded-text">
+                    Maintain Professionalism
+                  </span>
+                  - Even if the feedback feels unfair, responding with
+                  professionalism helps maintain your reputation and builds
+                  trust with other users.
+                </li>
+              </ol>
+              <p className="ratings-conflict-resolution-page__mobile-guidelines-for-receiving-negative-ratings-or-feedback-bottom-description">
+                These guidelines can help you handle negative ratings in a
+                constructive and proactive way, turning a difficult situation
+                into an opportunity for growth and improvement. Continue to Step
+                1 below if you would like to resolve a conflict with ratings or
+                feedback.
+              </p>
+            </div>
+            <div className="ratings-conflict-resolution-page__mobile-step-1-container">
+              <p className="ratings-conflict-resolution-page__mobile-step-1-container-inner-text">
+                <span className="ratings-conflict-resolution-page__mobile-step-1-container-bolded-text">
+                  Step 1:
+                </span>
+                <span className="ratings-conflict-resolution-page__mobile-step-1-container-description">
+                  Identify what type of conflict you are experiencing
+                </span>
+              </p>
+            </div>
+            <p className="ratings-conflict-resolution-page__mobile-which-path-describes-your-issue-header">
+              Which Path Describes Your Issue?
+            </p>
+            <div className="ratings-conflict-resolution-page__mobile-would-like-to-leave-negative-feedback-container">
+              <p className="ratings-conflict-resolution-page__mobile-would-like-to-leave-negative-feedback-header">
+                I would like to leave or have left a Negative Rating or Feedback
+              </p>
+              <p className="ratings-conflict-resolution-page__mobile-would-like-to-leave-negative-feedback-description">
+                Before submitting negative feedback, we encourage you to
+                carefully reconsider and ensure it is fair and accurate. It's
+                important to approach this thoughtfully, focusing on specific
+                points that led to your dissatisfaction. Consider first
+                addressing any concerns directly with the other party in a
+                professional and respectful manner. By doing so, you allow both
+                sides the opportunity to resolve the issue and maintain the
+                integrity of our review system. Submitting negative feedback can
+                have lasting effects, so please be sure it reflects your
+                experience accurately and constructively.
+              </p>
+              <div className="ratings-conflict-resolution-page__mobile-would-like-to-leave-negative-feedback-button-container">
+                <button className="ratings-conflict-resolution-page__mobile-this-describes-my-issue-button">
+                  This describes my issue
+                </button>
+                <button className="ratings-conflict-resolution-page__mobile-learn-more-about-ratings-button">
+                  Learn more about Ratings
+                </button>
+              </div>
+            </div>
+            <div className="ratings-conflict-resolution-page__mobile-have-received-negative-rating-or-feedback-container">
+              <p className="ratings-conflict-resolution-page__mobile-have-received-negative-rating-or-feedback-header">
+                I have received a Negative Rating or Feedback
+              </p>
+              <p className="ratings-conflict-resolution-page__mobile-have-received-negative-rating-or-feedback-description">
+                If you believe a rating or feedback is inaccurate or unfair, we
+                encourage you to address it directly with the Agency and using
+                professionalism and clarity. Focus on the specific points you
+                dispute, staying respectful and factual rather than expressing
+                frustration. By providing precise information, you can help
+                ensure the situation is resolved fairly. Taking initiative in
+                addressing concerns helps maintain the integrity of the review
+                system for everyone involved.
+              </p>
+              <div className="ratings-conflict-resolution-page__mobile-have-received-negative-rating-or-feedback-button-container">
+                <button className="ratings-conflict-resolution-page__mobile-this-describes-my-issue-button">
+                  This describes my issue
+                </button>
+                <button className="ratings-conflict-resolution-page__mobile-learn-more-about-ratings-button">
+                  Learn more about Ratings
+                </button>
+              </div>
             </div>
           </div>
         </div>
