a7606f012737f21e76597a50bc7f869dab2543fc
Alireza Entessari <alintssr@gmail.com>
2024-12-23 13:39:52 -0500

    Added icons to dashboard

--- src/pages/DashboardHomePage/DashboardHomePage.js
+++ src/pages/DashboardHomePage/DashboardHomePage.js
@@ -5,10 +5,17 @@ import HeaderWithProfilePic from "../../components/HeaderWithProfilePic/HeaderWi
 import SearchIcon from "../../assets/icons/Search (2).svg";
 import NotificationsIcon from "../../assets/icons/notificationbellmessage.svg";
 import CloseIcon from "../../assets/icons/xtask.svg";
-import OrangeDotIcon from "../../assets/icons/COffersDot.svg";
+import OrangeDotIcon from "../../assets/icons/orangedot.svg";
 import VerticalSliderMessagesIcon from "../../assets/icons/verticalslidermessages.svg";
 import MessagesFourIcon from "../../assets/icons/messagesfour.svg";
 import CreateMessageIcon from "../../assets/icons/createmessage.svg";
+import MobileCalendarIcon from "../../assets/icons/mobilecalendar.svg";
+import TaskIconOneIcon from "../../assets/icons/taskiconone.svg";
+import GrayCheckmarkIcon from "../../assets/icons/graycheckmark.svg";
+import OrangeCheckmarkIcon from "../../assets/icons/orangecheckmark.svg";
+import GrayDotIcon from "../../assets/icons/graydot.svg";
+import BlueDashDividerIcon from "../../assets/icons/bluedashdivider.svg";
+import OrangeDashDividerIcon from "../../assets/icons/orangedashdivider.svg";
 
 export default function DashboardHomePage() {
   return (
@@ -123,6 +130,7 @@ export default function DashboardHomePage() {
                     </tr>
                   </thead>
                   <tbody className="dashboard-home-page__mobile-messages-table-body-section">
+                    <div className="dashboard-home-page__mobile-messages-table-white-space"></div>
                     <tr className="dashboard-home-page__mobile-messages-table-data-row-1">
                       <td className="dashboard-home-page__mobile-messages-table-data-row-1-date">
                         <img
@@ -146,6 +154,7 @@ export default function DashboardHomePage() {
                         />
                       </td>
                     </tr>
+                    <div className="dashboard-home-page__mobile-messages-table-white-space"></div>
                     <tr className="dashboard-home-page__mobile-messages-table-data-row-2">
                       <td className="dashboard-home-page__mobile-messages-table-data-row-2-date">
                         <img
@@ -161,8 +170,58 @@ export default function DashboardHomePage() {
                         ABC Staffing Inc.
                       </td>
                       <td className="dashboard-home-page__mobile-messages-table-data-row-2-subject">
-                        Checking in...  <img
+                        Checking in...{" "}
+                        <img
                           className="dashboard-home-page__mobile-messages-close-icon-2"
+                          src={CloseIcon}
+                          alt="Close Icon"
+                        />
+                      </td>
+                    </tr>
+                    <div className="dashboard-home-page__mobile-messages-table-white-space"></div>
+                    <tr className="dashboard-home-page__mobile-messages-table-data-row-3">
+                      <td className="dashboard-home-page__mobile-messages-table-data-row-3-date">
+                        <img
+                          className="dashboard-home-page__mobile-orange-dot-icon"
+                          src={OrangeDotIcon}
+                          alt="Orange Dot"
+                        />
+                        <span className="dashboard-home-page__mobile-messages-table-data-row-3-date-text">
+                          Fri 7/19
+                        </span>
+                      </td>
+                      <td className="dashboard-home-page__mobile-messages-table-data-row-3-sender">
+                        Beta Staffing Agency
+                      </td>
+                      <td className="dashboard-home-page__mobile-messages-table-data-row-3-subject">
+                        Partnership...
+                        <img
+                          className="dashboard-home-page__mobile-messages-close-icon-3"
+                          src={CloseIcon}
+                          alt="Close Icon"
+                        />
+                      </td>
+                    </tr>
+                    <div className="dashboard-home-page__mobile-messages-table-white-space"></div>
+                    <tr className="dashboard-home-page__mobile-messages-table-data-row-4">
+                      <td className="dashboard-home-page__mobile-messages-table-data-row-4-date">
+                        {" "}
+                        <img
+                          className="dashboard-home-page__mobile-orange-dot-icon"
+                          src={OrangeDotIcon}
+                          alt="Orange Dot"
+                        />
+                        <span className="dashboard-home-page__mobile-messages-table-data-row-4-date-text">
+                          Fri 7/19
+                        </span>
+                      </td>
+                      <td className="dashboard-home-page__mobile-messages-table-data-row-4-sender">
+                        Circular Staffing
+                      </td>
+                      <td className="dashboard-home-page__mobile-messages-table-data-row-4-subject">
+                        Re: Partnersh...
+                        <img
+                          className="dashboard-home-page__mobile-messages-close-icon-4"
                           src={CloseIcon}
                           alt="Close Icon"
                         />
@@ -170,6 +229,136 @@ export default function DashboardHomePage() {
                     </tr>
                   </tbody>
                 </table>
+                <img
+                  className="dashboard-home-page__mobile-messages-vertical-slider-messages-icon"
+                  src={VerticalSliderMessagesIcon}
+                  alt="Vertical Slider Messages Icon"
+                />
+              </div>
+              <img
+                className="dashboard-home-page__mobile-mobile-calendar-icon"
+                src={MobileCalendarIcon}
+                alt="Mobile Calendar Icon"
+              />
+              <div className="dashboard-home-page__mobile-tasks-container">
+                <div className="dashboard-home-page__mobile-tasks-header-container">
+                  <p className="dashboard-home-page__mobile-tasks-header">
+                    Tasks
+                  </p>
+                  <img
+                    className="dashboard-home-page__mobile-task-icon-one-icon"
+                    src={TaskIconOneIcon}
+                    alt="Task Icon One Icon"
+                  />
+                </div>
+                <table className="dashboard-home-page__mobile-tasks-table">
+                  <thead className="dashboard-home-page__mobile-tasks-table-header-section">
+                    <tr className="dashboard-home-page__mobile-tasks-table-header-row-1">
+                      <th className=""></th>
+                    </tr>
+                  </thead>
+                  <tbody className="dashboard-home-page__mobile-tasks-body-section">
+                    <tr className="dashboard-home-page__mobile-tasks-data-row-1">
+                      <td className="dashboard-home-page__mobile-tasks-data-row-1-date">
+                        <img
+                          className="dashboard-home-page__mobile-orange-dot-icon"
+                          src={OrangeDotIcon}
+                          alt="Orange Dot Icon"
+                        />
+                        <span className="dashboard-home-page__mobile-tasks-data-row-1-date-text">
+                          9/13/24
+                        </span>
+                      </td>
+                      <td className="dashboard-home-page__mobile-tasks-data-row-1-tasks">
+                        Call Kelly Re: New Applicant{" "}
+                        <img
+                          className="dashboard-home-page__mobile-tasks-gray-checkmark-icon"
+                          src={GrayCheckmarkIcon}
+                          alt="Gray Checkmark Icon"
+                        />
+                        <img
+                          className="dashboard-home-page__mobile-tasks-close-icon"
+                          src={CloseIcon}
+                          alt="Close Icon"
+                        />
+                      </td>
+                    </tr>
+                    <div className="dashboard-home-page__mobile-tasks-white-space"></div>
+                    <tr className="dashboard-home-page__mobile-tasks-data-row-2">
+                      <td className="dashboard-home-page__mobile-tasks-data-row-2-date">
+                        <img
+                          className="dashboard-home-page__mobile-tasks-gray-dot-icon"
+                          src={GrayDotIcon}
+                          alt="Gray Dot Icon"
+                        />
+                        <span className="dashboard-home-page__mobile-tasks-data-row-2-date-text">
+                          9/10/24
+                        </span>
+                      </td>
+                      <td className="dashboard-home-page__mobile-tasks-data-row-2-tasks">
+                        Call Jerry about Contract{" "}
+                        <img
+                          className="dashboard-home-page__mobile-orange-checkmark-icon"
+                          src={OrangeCheckmarkIcon}
+                          alt="Orange Checkmark Icon"
+                        />
+                        <img
+                          className="dashboard-home-page__mobile-tasks-close-icon"
+                          src={CloseIcon}
+                          alt="Close Icon"
+                        />
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <img
+                  className="dashboard-home-page__mobile-tasks-vertical-slider-messages-icon"
+                  src={VerticalSliderMessagesIcon}
+                  alt="Vertical Slider Messages Icon"
+                />
+              </div>
+              <div className="dashboard-home-page__mobile-activity-summary-container">
+                <div className="dashboard-home-page__mobile-activity-summary-top-container">
+                  <p className="dashboard-home-page__mobile-activity-summary-header">
+                    Activity Summary
+                  </p>
+                  <div className="dashboard-home-page__mobile-activity-summary-jobs-candidates-placements-container">
+                    <div className="dashboard-home-page__mobile-activity-summary-jobs-container">
+                      <p className="dashboard-home-page__mobile-activity-summary-jobs-text">
+                        Jobs
+                      </p>
+                      <p className="dashboard-home-page__mobile-activity-summary-jobs-number">
+                        17
+                      </p>
+                    </div>
+                    <div className="dashboard-home-page__mobile-activity-summary-candidates-container">
+                      <p className="dashboard-home-page__mobile-activity-summary-candidates-text">
+                        Candidates
+                      </p>
+                      <p className="dashboard-home-page__mobile-activity-summary-candidates-number">
+                        12
+                      </p>
+                    </div>
+                    <div className="dashboard-home-page__mobile-activity-summary-placements-container">
+                      <p className="dashboard-home-page__mobile-activity-summary-placements-text">
+                        Placements
+                      </p>
+                      <p className="dashboard-home-page__mobile-activity-summary-placements-number">
+                        29
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="dashboard-home-page__mobile-activity-summary-my-jobs-container">
+                  <p className="dashboard-home-page__mobile-activity-summary-my-jobs-text">
+                    MY JOBS
+                  </p>
+                  <img
+                    className="dashboard-home-page__mobile-blue-dash-divider-icon"
+                    src={BlueDashDividerIcon}
+                    alt="Blue Dash Divider Icon"
+                  />
+                </div>
               </div>
             </div>
           </div>
