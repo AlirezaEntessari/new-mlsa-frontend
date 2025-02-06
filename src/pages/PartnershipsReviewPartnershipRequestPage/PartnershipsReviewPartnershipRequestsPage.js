2d0d9372235f94602b6725202ccd0b67f9a94e82
Alireza Entessari <alintssr@gmail.com>
2025-02-06 13:37:11 -0500

    Added tablet styling to Partnerships Review Partnership Request Page

--- src/pages/PartnershipsReviewPartnershipRequestPage/PartnershipsReviewPartnershipRequestsPage.js
+++ src/pages/PartnershipsReviewPartnershipRequestPage/PartnershipsReviewPartnershipRequestsPage.js
@@ -416,6 +416,211 @@ export default function PartnershipsReviewPartnershipRequestsPage() {
               <ModalPartnershipAgreement closeModal={closeModal} />
             )}
           </div>
+          <div className="partnerships-review-partnership-requests-page__tablet-desktop-container">
+            <p className="partnerships-review-partnership-requests-page__tablet-desktop-header">
+              Partnerships
+            </p>
+            <PartnershipsNavbar />
+            <p className="partnerships-review-partnership-requests-page__tablet-desktop-review-partnership-request">
+              Review Partnership Request
+            </p>
+            <div className="partnerships-review-partnership-requests-page__tablet-desktop-lmn-staffing-inc-container">
+              <img
+                className="partnerships-review-partnership-requests-page__tablet-desktop-lmn-staffing-inc-logo"
+                src={LMNStaffingIncLogo}
+                alt="LMN Staffing Inc Logo"
+              />
+              <p className="partnerships-review-partnership-requests-page__tablet-desktop-lmn-staffing-inc-text">
+                LMN Staffing Inc.
+              </p>
+              <div className="partnerships-review-partnership-requests-page__tablet-desktop-lmn-staffing-inc-reviews-container">
+                <div className="partnerships-review-partnership-requests-page__tablet-desktop-lmn-staffing-inc-star-container">
+                  <img
+                    className="partnerships-review-partnership-requests-page__tablet-desktop-rating-star-icon-1"
+                    src={RatingStarIcon}
+                    alt="Rating Star Icon"
+                  />
+                  <img
+                    className="partnerships-review-partnership-requests-page__tablet-desktop-rating-star-icon"
+                    src={RatingStarIcon}
+                    alt="Rating Star Icon"
+                  />
+                  <img
+                    className="partnerships-review-partnership-requests-page__tablet-desktop-rating-star-icon"
+                    src={RatingStarIcon}
+                    alt="Rating Star Icon"
+                  />
+                  <img
+                    className="partnerships-review-partnership-requests-page__tablet-desktop-rating-star-icon"
+                    src={RatingStarIcon}
+                    alt="Rating Star Icon"
+                  />
+                  <img
+                    className="partnerships-review-partnership-requests-page__tablet-desktop-rating-star-icon"
+                    src={RatingStarIcon}
+                    alt="Rating Star Icon"
+                  />
+                </div>
+                <p className="partnerships-review-partnership-requests-page__tablet-desktop-lmn-staffing-inc-desktop-rating-text">
+                  5
+                </p>
+              </div>
+              <div className="partnerships-review-partnership-requests-page__tablet-desktop-lmn-staffing-inc-active-jobs-active-candidates-container">
+                <div className="partnerships-review-partnership-requests-page__tablet-desktop-lmn-staffing-inc-active-jobs-container">
+                  <p className="partnerships-review-partnership-requests-page__tablet-desktop-active-jobs-text">
+                    Active Jobs
+                  </p>
+                  <p className="partnerships-review-partnership-requests-page__tablet-desktop-active-jobs-number">
+                    17
+                  </p>
+                </div>
+                <div className="partnerships-review-partnership-requests-page__tablet-desktop-lmn-staffing-inc-active-candidates-container">
+                  <p className="partnerships-review-partnership-requests-page__tablet-desktop-active-candidates-text">
+                    Active Candidates
+                  </p>
+                  <p className="partnerships-review-partnership-requests-page__tablet-desktop-active-candidates-number">
+                    4
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="partnerships-review-partnership-requests-page__tablet-desktop-instructions-for-signing-partnership-agreement-container">
+              <p className="partnerships-review-partnership-requests-page__tablet-desktop-instructions-for-signing-partnership-agreement-header">
+                Instructions for Signing the Partnership Agreement
+              </p>
+              <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-instructions-before-you-proceed">
+                Before you proceed with signing the Partnership Agreement,
+                please take the following steps to ensure everything is
+                completed correctly.
+              </p>
+              <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-instructions-read-carefully">
+                1.{" "}
+                <span className="partnerships-review-partnership-requests-page__tablet-desktop-bolded-text">
+                  **Read Carefully.**
+                </span>{" "}
+                Review the entire Agreement and Terms and Conditions thoroughly.
+                Make sure you fully understand each section, including the
+                responsibilities, profit-sharing terms, confidentiality
+                requirements, and termination conditions.
+              </p>
+              <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-instructions-seek-clarification">
+                2.{" "}
+                <span className="partnerships-review-partnership-requests-page__tablet-desktop-bolded-text">
+                  **Seek Clarification.**
+                </span>{" "}
+                If there is any part of the Agreement that is unclear or if you
+                have any questions, please consult with your legal advisor or
+                contact the other Party to discuss and clarify any points before
+                signing.
+              </p>
+              <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-instructions-verify-details">
+                3.{" "}
+                <span className="partnerships-review-partnership-requests-page__tablet-desktop-bolded-text">
+                  **Verify Details.**
+                </span>{" "}
+                Double-check that all the information, such as your name, title,
+                and the date, is accurate and correctly filled out in the
+                designated sections.
+              </p>
+              <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-instructions-signature">
+                4.{" "}
+                <span className="partnerships-review-partnership-requests-page__tablet-desktop-bolded-text">
+                  **Signature.**
+                </span>{" "}
+                When you are ready to sign, please do so in the space provided
+                below your name and title. Your signature should be clear and
+                legible.
+              </p>
+              <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-instructions-date">
+                5.{" "}
+                <span className="partnerships-review-partnership-requests-page__tablet-desktop-bolded-text">
+                  **Date.**
+                </span>{" "}
+                Ensure you include the date of signing next to your signature.
+                This date should match the day you actually sign the document.
+              </p>
+              <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-instructions-by-following-these-instructions">
+                By following these instructions, you help ensure the Agreement
+                is executed properly, making it a valid and binding document for
+                all Parties involved.
+              </p>
+            </div>
+            <div className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-container">
+              <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-header">
+                PARTNERSHIP AGREEMENT
+              </p>
+              <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-introduction">
+                This Partnership Agreement ("Agreement") is made and entered
+                into as of [Date], by and between [Agency 1 Name], a [State]
+                [Type of Business Entity] with its principal office located at
+                [Agency 1 Address], and [Agency 2 Name], a [State] [Type of
+                Business Entity] with its principal office located at [Agency 2
+                Address] (collectively referred to as the "Parties" and
+                individually as a "Party").
+              </p>
+              <div className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-purpose-container">
+                <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-purpose-header">
+                  1. Purpose
+                </p>
+                <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-purpose-description">
+                  The purpose of this Agreement is to establish a partnership
+                  between the Parties for the mutual benefit of both agencies.
+                  The Parties agree to collaborate and share resources,
+                  expertise, and information to enhance their service offerings
+                  and achieve common business objectives.
+                </p>
+              </div>
+              <div className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-responsibilities-of-the-parties-container">
+                <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-responsibilities-of-the-parties-header">
+                  2. Responsibilities of the Parties
+                </p>
+                <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-responsibilities-of-the-parties-agency-1">
+                  -**[Agency 1 Name]**[Agency 1 Name] agrees to provide
+                  [specific services, resources, or contributions] as part of
+                  this partnership.
+                </p>
+                <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-responsibilities-of-the-parties-agency-2">
+                  -**[Agency 2 Name]**[Agency 2 Name] agrees to provide
+                  [specific services, resources, or contributions] as part of
+                  this partnership.
+                </p>
+              </div>
+              <div className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-term-container">
+                <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-term-header">
+                  3. Term
+                </p>
+                <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-term-description">
+                  This Agreement shall commence on the date of execution and
+                  shall continue in effect until terminated by either Party in
+                  accordance with the terms of this Agreement.
+                </p>
+              </div>
+              <div className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-confidentiality-container">
+                <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-confidentiality-header">
+                  4. Confidentiality
+                </p>
+                <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-confidentiality-description">
+                  The Parties agree to maintain the confidentiality of any
+                  proprietary or confidential information disclosed during the
+                  course of this partnership. Neither Party shall use or
+                  disclose such information for any purpose other than as
+                  necessary to fulfill the obligations under this Agreement.
+                </p>
+              </div>
+              <div className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-profit-sharing-container">
+                <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-profit-sharing-header">
+                  5. Profit Sharing
+                </p>
+                <p className="partnerships-review-partnership-requests-page__tablet-desktop-partnership-agreement-profit-sharing-description">
+                  The Parties agree to share any profits or revenue generated as
+                  a result of this partnership in the following manner: [Specify
+                  profit-sharing ratio or terms]. Payments shall be made on a
+                  [monthly/quarterly] basis, with detailed accounting provided
+                  to each Party.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
